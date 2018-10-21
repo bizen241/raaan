6a0af7818a945b1b70b6c1699c4ebf8fc41a6d07
bizen241 <bizen241@gmail.com>
2018-10-21 15:08:17 +0900

    chore(server): send index.html

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,7 +1,14 @@
 import * as express from "express";
+import { join } from "path";
+
+const htmlPath = join(process.cwd(), "assets/index.html");
 
 export const createApp = () => {
   const app = express();
+
+  app.get("*", (_, res) => {
+    res.sendFile(htmlPath);
+  });
 
   return app;
 };
