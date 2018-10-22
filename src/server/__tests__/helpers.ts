4e0f821162a8f5e0723684af4cea62d112fe269f
bizen241 <bizen241@gmail.com>
2018-10-22 11:54:00 +0900

    test(server): add testProcessEnv

--- src/server/__tests__/helpers.ts
+++ src/server/__tests__/helpers.ts
@@ -1,17 +1,25 @@
 import { Server } from "http";
 import nodeFetch, { RequestInit } from "node-fetch";
 import { createApp } from "../app";
+import { ProcessEnv } from "../env";
 
-const app = createApp();
-const port = 5000;
-const host = "localhost";
-const base = `http://${host}:${port}`;
+const serverPort = 5000;
+const serverHost = "localhost";
+
+export const testProcessEnv: ProcessEnv = {
+  serverPort,
+  serverHost
+};
+
+const base = `http://${serverHost}:${serverPort}`;
 
 export const fetch = (path: string, init?: RequestInit) => {
   const url = new URL(path, base);
 
   return nodeFetch(url.toString(), init);
 };
+
+const app = createApp();
 
 export class TestServer {
   server?: Server;
@@ -26,7 +34,7 @@ export class TestServer {
 
   startServer() {
     return new Promise(resolve => {
-      this.server = app.listen(port, host, () => resolve());
+      this.server = app.listen(serverPort, serverHost, () => resolve());
     });
   }
 
