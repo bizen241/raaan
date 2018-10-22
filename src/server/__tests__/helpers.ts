10e2300de5e4d7f96a6c4be2c119522f97b0fc3b
bizen241 <bizen241@gmail.com>
2018-10-23 08:32:36 +0900

    chore(server): add databaseUrl to ProcessEnv

--- src/server/__tests__/helpers.ts
+++ src/server/__tests__/helpers.ts
@@ -1,9 +1,16 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import { ProcessEnv } from "../env";
 
-const serverPort = 5000;
-const serverHost = "localhost";
+const { TEST_DATABASE_URL } = process.env;
+
+if (TEST_DATABASE_URL === undefined) {
+  throw new Error("TEST_DATABASE_URL is not defined");
+}
 
 export const testProcessEnv: ProcessEnv = {
-  serverPort,
-  serverHost
+  serverPort: 5000,
+  serverHost: "localhost",
+  databaseUrl: TEST_DATABASE_URL
 };
--- src/server/env.ts
+++ src/server/env.ts
@@ -4,13 +4,19 @@ dotenv.config();
 export interface ProcessEnv {
   serverPort: number;
   serverHost: string;
+  databaseUrl: string;
 }
 
 export const getProcessEnv = (): ProcessEnv => {
-  const { SERVER_PORT, SERVER_HOST } = process.env;
+  const { SERVER_PORT, SERVER_HOST, DATABASE_URL } = process.env;
+
+  if (DATABASE_URL === undefined) {
+    throw new Error("DATABASE_URL is not defined");
+  }
 
   return {
     serverPort: SERVER_PORT !== undefined && !isNaN(Number(SERVER_PORT)) ? Number(SERVER_PORT) : 3000,
-    serverHost: SERVER_HOST || "localhost"
+    serverHost: SERVER_HOST || "localhost",
+    databaseUrl: DATABASE_URL
   };
 };
