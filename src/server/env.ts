18a6abeb9ad68f1ffa6ea8474a19665b174111f6
bizen241 <bizen241@gmail.com>
2018-10-22 12:35:36 +0900

    chore(server): fill ProcessEnv with default value

--- src/server/env.ts
+++ src/server/env.ts
@@ -2,15 +2,15 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export interface ProcessEnv {
-  serverPort: number | undefined;
-  serverHost: string | undefined;
+  serverPort: number;
+  serverHost: string;
 }
 
 export const getProcessEnv = (): ProcessEnv => {
   const { SERVER_PORT, SERVER_HOST } = process.env;
 
   return {
-    serverPort: SERVER_PORT !== undefined && !isNaN(Number(SERVER_PORT)) ? Number(SERVER_PORT) : undefined,
-    serverHost: SERVER_HOST
+    serverPort: SERVER_PORT !== undefined && !isNaN(Number(SERVER_PORT)) ? Number(SERVER_PORT) : 3000,
+    serverHost: SERVER_HOST || "localhost"
   };
 };
