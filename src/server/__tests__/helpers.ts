3edcc43b8b9dc6bbdb6dba1f9b1be6a216053793
bizen241 <bizen241@gmail.com>
2018-10-22 16:17:49 +0900

    test(server): delete unused helpers

--- src/server/__tests__/helpers.ts
+++ src/server/__tests__/helpers.ts
@@ -1,6 +1,3 @@
-import { Server } from "http";
-import nodeFetch, { RequestInit } from "node-fetch";
-import { createApp } from "../app";
 import { ProcessEnv } from "../env";
 
 const serverPort = 5000;
@@ -10,41 +7,3 @@ export const testProcessEnv: ProcessEnv = {
   serverPort,
   serverHost
 };
-
-const base = `http://${serverHost}:${serverPort}`;
-
-export const testFetch = (path: string, init?: RequestInit) => {
-  const url = new URL(path, base);
-
-  return nodeFetch(url.toString(), init);
-};
-
-const app = createApp();
-
-export class TestServer {
-  server?: Server;
-
-  async start() {
-    await this.startServer();
-  }
-
-  async stop() {
-    await this.stopServer();
-  }
-
-  startServer() {
-    return new Promise(resolve => {
-      this.server = app.listen(serverPort, serverHost, () => resolve());
-    });
-  }
-
-  stopServer() {
-    return new Promise(resolve => {
-      if (this.server === undefined) {
-        resolve();
-      } else {
-        this.server.close(() => resolve());
-      }
-    });
-  }
-}
