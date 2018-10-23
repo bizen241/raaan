1e372b1aac79352cd89d4f4bcafbf593260e2d6b
bizen241 <bizen241@gmail.com>
2018-10-23 09:45:49 +0900

    test(server): modify test for start

--- src/server/__tests__/start.test.ts
+++ src/server/__tests__/start.test.ts
@@ -1,16 +1,15 @@
 import { start } from "../start";
 import { testProcessEnv } from "./helpers";
 
-const spyLog = jest.spyOn(console, "log");
-spyLog.mockImplementation(message => message);
-
 test("start", async () => {
+  const spyLog = jest.spyOn(console, "log");
+  spyLog.mockImplementation(message => message);
+
   const server = await start(testProcessEnv);
 
-  expect(console.log).toBeCalled();
+  expect(spyLog).toBeCalled();
   expect(spyLog.mock.results[0].value).toEqual(`server: listening on port ${testProcessEnv.serverPort}`);
 
-  spyLog.mockReset();
   spyLog.mockRestore();
 
   return new Promise(resolve => {
