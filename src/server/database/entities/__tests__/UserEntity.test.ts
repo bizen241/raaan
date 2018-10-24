36234f56f5c9e244436cb5370bc5746bca8b44cb
bizen241 <bizen241@gmail.com>
2018-10-25 08:16:21 +0900

    test(db): add test for UserEntity

--- src/server/database/entities/__tests__/UserEntity.test.ts
+++ src/server/database/entities/__tests__/UserEntity.test.ts
@@ -1,4 +1,5 @@
 import { getManager } from "typeorm";
+import * as uuid from "uuid";
 import { TestDatabase } from "../../__tests__/TestDatabase";
 import { createUser, UserEntity } from "../UserEntity";
 
@@ -14,7 +15,7 @@ afterAll(async () => {
   await testDatabase.close();
 });
 
-test("users", async () => {
+test("without id", async () => {
   const userRepository = getManager().getRepository(UserEntity);
   const user = createUser({ name: "name", role: "Admin" });
   await userRepository.save(user);
@@ -22,4 +23,15 @@ test("users", async () => {
   const users = await userRepository.find();
 
   expect(users.length).toBe(1);
+});
+
+test("with id", async () => {
+  const userRepository = getManager().getRepository(UserEntity);
+  const userId = uuid();
+  const user = createUser({ id: userId, name: "name", role: "Admin" });
+  await userRepository.save(user);
+
+  const savedUser = await userRepository.findOne(userId);
+
+  expect(savedUser && savedUser.id).toBe(userId);
 });
