aa95b7b5dd60d89e751f6ca03fe70de45317e409
bizen241 <bizen241@gmail.com>
2018-10-25 09:43:43 +0900

    test(server): delete testFetch

--- src/server/routes/__tests__/fallback.test.ts
+++ src/server/routes/__tests__/fallback.test.ts
@@ -1,4 +1,4 @@
-import { testFetch, TestServer } from "./helpers";
+import { TestServer } from "./helpers";
 
 const testServer = new TestServer();
 
@@ -10,14 +10,14 @@ afterAll(async () => {
 });
 
 test("home", async () => {
-  const response = await testFetch("/");
+  const response = await testServer.fetch("/");
 
   expect(response.status).toEqual(200);
   expect(response.headers.get("content-type")).toContain("text/html");
 });
 
 test("help", async () => {
-  const response = await testFetch("/help");
+  const response = await testServer.fetch("/help");
 
   expect(response.status).toEqual(200);
   expect(response.headers.get("content-type")).toContain("text/html");
--- src/server/routes/__tests__/helpers.ts
+++ src/server/routes/__tests__/helpers.ts
@@ -1,22 +1,20 @@
 import { createServer } from "http";
-import nodeFetch, { RequestInit } from "node-fetch";
+import fetch, { RequestInit } from "node-fetch";
 import { testProcessEnv } from "../../__tests__/helpers";
 import { createApp } from "../../app";
 
 const { serverPort, serverHost } = testProcessEnv;
-
 const base = `http://${serverHost}:${serverPort}`;
-
-export const testFetch = (path: string, init?: RequestInit) => {
-  const url = new URL(path, base);
-
-  return nodeFetch(url.toString(), init);
-};
-
 const app = createApp();
 
 export class TestServer {
   server = createServer(app);
+
+  fetch(path: string, init?: RequestInit) {
+    const url = new URL(path, base);
+
+    return fetch(url.toString(), init);
+  }
 
   async start() {
     await this.startServer();
