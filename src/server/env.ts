26677e72ad5e14a1feeb7ebe5ba7cceec7cfdf0e
bizen241 <bizen241@gmail.com>
2018-10-26 21:51:32 +0900

    chore(server): add sessionSecret to ProcessEnv

--- src/server/env.ts
+++ src/server/env.ts
@@ -1,14 +1,16 @@
 import * as dotenv from "dotenv";
+import * as uuid from "uuid";
 dotenv.config();
 
 export interface ProcessEnv {
   serverPort: number;
   serverHost: string;
+  sessionSecret: string;
   databaseUrl: string;
 }
 
 export const getProcessEnv = (): ProcessEnv => {
-  const { SERVER_PORT, SERVER_HOST, DATABASE_URL } = process.env;
+  const { SERVER_PORT, SERVER_HOST, SESSION_SECRET, DATABASE_URL } = process.env;
 
   if (DATABASE_URL === undefined) {
     throw new Error("DATABASE_URL is not defined");
@@ -17,6 +19,7 @@ export const getProcessEnv = (): ProcessEnv => {
   return {
     serverPort: SERVER_PORT !== undefined && !isNaN(Number(SERVER_PORT)) ? Number(SERVER_PORT) : 3000,
     serverHost: SERVER_HOST || "localhost",
+    sessionSecret: SESSION_SECRET || uuid(),
     databaseUrl: DATABASE_URL
   };
 };
