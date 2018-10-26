192956a3da1d3449ae2b4d6663c66084ac8558f6
bizen241 <bizen241@gmail.com>
2018-10-26 21:32:44 +0900

    chore(entity): add some permissions

--- src/shared/entities/UserEntity.ts
+++ src/shared/entities/UserEntity.ts
@@ -1,6 +1,6 @@
 import { BaseEntity } from "./BaseEntity";
 
-export type Permission = "Owner" | "Admin" | "Write" | "Read";
+export type Permission = "Owner" | "Admin" | "Write" | "Read" | "Guest" | "Ghost";
 
 export interface UserEntity extends BaseEntity<"User"> {
   name: string;
