79aa0cc5e024957638e63bbaef93933ce250ba64
bizen241 <bizen241@gmail.com>
2018-10-27 10:01:11 +0900

    test(server): update test for createApp

--- src/server/__tests__/app.test.ts
+++ src/server/__tests__/app.test.ts
@@ -2,12 +2,12 @@ import * as express from "express";
 import { createApp } from "../app";
 import { testProcessEnv } from "./helpers";
 
-test("server", async () => {
+test("app", async () => {
   const app = express();
 
   const spyUse = jest.spyOn(app, "use");
 
   createApp(testProcessEnv, app);
 
-  expect(spyUse).toBeCalledTimes(1);
+  expect(spyUse).toBeCalledTimes(2);
 });
