bfd70d00df3ebe9e37a64f601ff01ca77d7349e1
bizen241 <bizen241@gmail.com>
2018-10-27 17:50:18 +0900

    test(server): TestServer receives app

--- src/server/routes/__tests__/helpers.ts
+++ src/server/routes/__tests__/helpers.ts
@@ -1,14 +1,17 @@
-import { createServer } from "http";
+import { createServer, Server } from "http";
 import fetch, { RequestInit } from "node-fetch";
 import { testProcessEnv } from "../../__tests__/helpers";
 import { createApp } from "../../app";
 
 const { serverPort, serverHost } = testProcessEnv;
 const base = `http://${serverHost}:${serverPort}`;
-const app = createApp(testProcessEnv);
 
 export class TestServer {
-  server = createServer(app);
+  server: Server;
+
+  constructor(app = createApp(testProcessEnv)) {
+    this.server = createServer(app);
+  }
 
   fetch(path: string, init?: RequestInit) {
     const url = new URL(path, base);
