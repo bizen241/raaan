369ee901d07a42d55fda1e291a0e0d2975d50aa5
bizen241 <bizen241@gmail.com>
2018-10-28 12:30:04 +0900

    chore(db): add AccountEntity

---
+++ src/server/database/entities/AccountEntity.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, ManyToOne } from "typeorm";
+import { AuthProviderName } from "../../../shared/auth";
+import { BaseEntity, BaseEntityConstructor } from "./BaseEntity";
+import { UserEntity } from "./UserEntity";
+
+interface AccountConstructor extends BaseEntityConstructor {
+  user: UserEntity;
+  provider: AuthProviderName;
+  accountId: string;
+}
+
+@Entity("accounts")
+export class AccountEntity extends BaseEntity<"Account"> {
+  type: "Account" = "Account";
+
+  @ManyToOne(() => UserEntity)
+  user!: UserEntity;
+
+  @Column()
+  provider!: AuthProviderName;
+
+  @Column()
+  accountId!: string;
+
+  constructor({ id, user, provider, accountId }: Partial<AccountConstructor> = {}) {
+    super(id);
+
+    if (user !== undefined) {
+      this.user = user;
+    }
+    if (provider !== undefined) {
+      this.provider = provider;
+    }
+    if (accountId !== undefined) {
+      this.accountId = accountId;
+    }
+  }
+}
+
+export const createAccount = (params: AccountConstructor) => new AccountEntity(params);
