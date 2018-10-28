069c8330ce510619204bb96e84008e88fc88c214
bizen241 <bizen241@gmail.com>
2018-10-28 16:07:46 +0900

    chore(server): use auth middleware and router

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,15 @@
 import * as express from "express";
+import { createAuthMiddleware } from "./auth";
 import { ProcessEnv } from "./env";
+import { authRouter } from "./routes/auth";
 import { fallbackRouter } from "./routes/fallback";
 import { createSessionMidleware } from "./session";
 
 export const createApp = (processEnv: ProcessEnv, app: express.Express = express()) => {
   app.use(createSessionMidleware(processEnv));
+  app.use(createAuthMiddleware(processEnv));
 
+  app.use("/auth", authRouter);
   app.use("*", fallbackRouter);
 
   return app;
