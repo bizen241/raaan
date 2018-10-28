84a162c839d58f94c4aa998bd4158e0adb2921e7
bizen241 <bizen241@gmail.com>
2018-10-28 10:40:24 +0900

    chore(auth): add properties for auth to NodeJS.ProcessEnv

--- src/typings/node.d.ts
+++ src/typings/node.d.ts
@@ -1,9 +1,11 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    DATABASE_URL?: string;
+    GITHUB_CLIENT_ID?: string;
+    GITHUB_CLIENT_SECRET?: string;
     NODE_ENV?: string;
     SERVER_HOST?: string;
     SERVER_PORT?: string;
     SESSION_SECRET?: string;
-    DATABASE_URL?: string;
   }
 }
