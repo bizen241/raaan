f9119b630fca4142c77ec760e807df9786f4097a
bizen241 <bizen241@gmail.com>
2018-10-28 10:17:50 +0900

    chore(auth): delete isAuthProviderName

--- src/shared/auth/index.ts
+++ src/shared/auth/index.ts
@@ -1,7 +1,1 @@
 export type AuthProviderName = "github";
-
-const authProviderNames = Object.values({
-  github: "github"
-} as { [P in AuthProviderName]: P });
-
-export const isAuthProviderName = (target: any): target is AuthProviderName => authProviderNames.includes(target);
