29ec45b3a7d3380c22c32cfcef4365ac6a891045
bizen241 <bizen241@gmail.com>
2018-10-29 11:42:44 +0900

    chore(api): add createApiDoc

---
+++ src/server/api/operation.ts
@@ -0,0 +1,36 @@
+import { OpenAPIV2 } from "openapi-types";
+import { Permission } from "../../shared/entities";
+
+type Tag = "user";
+
+interface Parameters {
+  summary: string;
+  tag: Tag;
+  permission: Permission;
+}
+
+const responses: OpenAPIV2.ResponsesObject = {
+  200: {
+    description: "OK",
+    schema: {
+      $ref: "#/definitions/Response"
+    }
+  },
+  default: {
+    description: "Error",
+    schema: {
+      $ref: "#/definitions/Error"
+    }
+  }
+};
+
+export const createApiDoc = ({ summary, tag, permission }: Parameters): OpenAPIV2.OperationObject => ({
+  summary,
+  tags: [tag],
+  responses,
+  security: [
+    {
+      [permission]: []
+    }
+  ]
+});
