cd2974ccb7dd63eaa6cc4e0c7b889ed3857b7dc7
bizen241 <bizen241@gmail.com>
2018-10-30 10:48:42 +0900

    chore(client): render app

---
+++ src/client/render.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export const renderApp = () => {
+  ReactDOM.render(<div>Welcome to Raaan!</div>, document.getElementById("root"));
+};
