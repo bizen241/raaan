c4f216961ebdd06bc4ba333ba6c157e73b5f8dae
bizen241 <bizen241@gmail.com>
2018-10-30 12:09:27 +0900

    chore(server): serve static files

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import { join } from "path";
+import * as serveStatic from "serve-static";
 import { prepareApi } from "./api";
 import { createAuthMiddleware } from "./auth";
 import { ProcessEnv } from "./env";
@@ -9,6 +11,8 @@ import { createSessionMidleware } from "./session";
 export const createApp = (processEnv: ProcessEnv, app: express.Express = express()) => {
   app.use(createSessionMidleware(processEnv));
   app.use(createAuthMiddleware(processEnv));
+
+  app.use(serveStatic(join(process.cwd(), "dist")));
 
   prepareApi(processEnv, app);
 
