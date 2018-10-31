cfc4b07f4a8731efe6c7f36559595b2db1980246
bizen241 <bizen241@gmail.com>
2018-10-31 22:43:49 +0900

    chore(intl): add locale

--- src/client/intl.ts
+++ src/client/intl.ts
@@ -1,6 +1,8 @@
 import { Catalogs } from "@lingui/core";
 import * as catalogEn from "../../assets/locales/en/messages";
+import * as catalogJa from "../../assets/locales/ja/messages";
 
 export const catalogs: Catalogs = {
-  en: catalogEn
+  en: catalogEn,
+  ja: catalogJa
 };
