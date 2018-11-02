d8570279f57bb1512d4ca2ca9f4b977c17c11496
bizen241 <bizen241@gmail.com>
2018-11-02 09:27:14 +0900

    chore(api): add EntityClass and EntityObject

--- src/shared/api/entities/index.ts
+++ src/shared/api/entities/index.ts
@@ -1,5 +1,11 @@
-export type EntityType = "Account" | "Session" | "User";
+import { Account } from "./Account";
+import { Session } from "./Session";
+import { User } from "./User";
 
 export * from "./Account";
 export * from "./Session";
 export * from "./User";
+
+export type EntityType = "Account" | "Session" | "User";
+
+export type EntityObject = Account | Session | User;
