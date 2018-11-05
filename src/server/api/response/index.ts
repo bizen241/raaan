7cc153e67e32e3f449a61b44b6c14a538ff170b7
bizen241 <bizen241@gmail.com>
2018-11-05 11:42:07 +0900

    chore(api): response search result

--- src/server/api/response/index.ts
+++ src/server/api/response/index.ts
@@ -1,5 +1,6 @@
 import { Response } from "express";
 import * as createError from "http-errors";
+import { entityCountPerPage, SearchResult } from "../../../shared/api/response/search";
 import { EntityClass } from "../../database/entities";
 import { normalizeEntities } from "./normalize";
 
@@ -8,6 +9,32 @@ export const responseFindResult = (res: Response, ...entities: EntityClass[]) =>
     const store = normalizeEntities(entities);
 
     res.status(200).json(store);
+  } catch (e) {
+    throw createError(500);
+  }
+};
+
+export const responseSearchResult = (res: Response, page: number, entities: EntityClass[]) => {
+  try {
+    const entityIds = entities.map(entity => entity.id);
+
+    const isFirstPage = page === 1;
+    const isEmpty = entityIds.length === 0;
+
+    const startIndex = isFirstPage ? 0 : 1;
+    const endIndex = startIndex + entityCountPerPage;
+
+    const hasPrev = !isFirstPage && !isEmpty;
+    const hasNext = entityIds.length - 1 === endIndex + 1;
+
+    const searchResult: SearchResult = {
+      ids: entityIds.slice(startIndex, endIndex),
+      prev: hasPrev ? null : entityIds[0],
+      next: hasNext ? null : entityIds[endIndex + 1],
+      entities: normalizeEntities(entities)
+    };
+
+    res.status(200).json(searchResult);
   } catch (e) {
     throw createError(500);
   }
