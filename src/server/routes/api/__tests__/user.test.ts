a5d78e72e1c76ed0b84c22fe9cad4b79177b0b57
bizen241 <bizen241@gmail.com>
2018-11-13 12:02:42 +0900

    test(api): update test for /user

--- src/server/routes/api/__tests__/user.test.ts
+++ src/server/routes/api/__tests__/user.test.ts
@@ -1,11 +1,27 @@
 import { createRequest, createResponse } from "node-mocks-http";
+import * as uuid from "uuid";
+import { createSession, createUser } from "../../../database/entities";
 import { GET } from "../user";
 
 test("user", async () => {
   const req = createRequest();
   const res = createResponse();
 
+  req.session = createSession({
+    user: createUser({
+      id: uuid(),
+      name: "Guest",
+      permission: "Guest"
+    }),
+    sessionId: uuid(),
+    expireAt: new Date(),
+    userAgent: "user-agent"
+  });
+
+  req.session.user.createdAt = new Date();
+  req.session.user.updatedAt = new Date();
+
   await GET(req, res, () => null);
 
-  expect(res.statusCode).toBe(404);
+  expect(res.statusCode).toBe(200);
 });
