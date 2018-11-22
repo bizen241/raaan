64108b3278eaeca161e088672ef6e00519a747f8
bizen241 <bizen241@gmail.com>
2018-11-22 09:29:33 +0900

    test(ci): encodeURIComponent

--- src/server/routes/api/__tests__/api.test.ts
+++ src/server/routes/api/__tests__/api.test.ts
@@ -1,71 +1,51 @@
 import { sign } from "cookie-signature";
 import { getManager } from "typeorm";
+import { Permission } from "../../../../shared/api/entities";
 import { testProcessEnv } from "../../../__tests__/helpers";
 import { TestServer } from "../../__tests__/helpers";
 import { sessions, users } from "./helpers";
 
-jest.setTimeout(10000);
-
 const testServer = new TestServer();
 
+const fetchAs = async (permission: Permission, method: string, path: string, body?: any) => {
+  const { sessionId } = sessions[permission];
+
+  return testServer.fetch(path, {
+    method,
+    body: body && JSON.stringify(body),
+    headers: {
+      Cookie: encodeURIComponent(`sid=${sign(sessionId, testProcessEnv.sessionSecret)}`)
+    }
+  });
+};
+
 beforeAll(async () => {
-  console.log("beforeAll:start", performance.now());
   await testServer.start();
 
   await getManager().save(Object.values(users));
   await getManager().save(Object.values(sessions));
-  console.log("beforeAll:end", performance.now());
 });
 afterAll(async () => {
-  console.log("afterAll:start", performance.now());
   await testServer.stop();
-  console.log("afterAll:end", performance.now());
 });
 
 test("GET /api/user failure", async () => {
-  console.log("firstTest:start", performance.now());
-  const { sessionId } = sessions.Guest;
-
-  const response = await testServer.fetch("/api/user", {
-    method: "GET",
-    headers: {
-      Cookie: `sid=${sign(sessionId, testProcessEnv.sessionSecret)}`
-    }
-  });
+  const response = await fetchAs("Guest", "GET", "/api/user");
 
   expect(response.status).toEqual(403);
   expect(response.headers.get("content-type")).toContain("text/html");
-  console.log("firstTest:end", performance.now());
 });
 
 test("GET /api/user success", async () => {
-  console.log("secondTest:start", performance.now());
-  const { sessionId } = sessions.Read;
-
-  const response = await testServer.fetch("/api/user", {
-    method: "GET",
-    headers: {
-      Cookie: `sid=${sign(sessionId, testProcessEnv.sessionSecret)}`
-    }
-  });
+  const response = await fetchAs("Read", "GET", "/api/user");
 
   expect(response.status).toEqual(200);
   expect(response.headers.get("content-type")).toContain("application/json");
-  console.log("secondTest:end", performance.now());
 });
 
 test("GET /api/users success", async () => {
-  console.log("thirdTest:start", performance.now());
-  const { sessionId } = sessions.Guest;
-
-  const response = await testServer.fetch("/api/users", {
-    method: "GET",
-    headers: {
-      Cookie: `sid=${sign(sessionId, testProcessEnv.sessionSecret)}`
-    }
-  });
+  const response = await fetchAs("Guest", "GET", "/api/users");
 
   expect(response.status).toEqual(200);
   expect(response.headers.get("content-type")).toContain("application/json");
-  console.log("thirdTest:end", performance.now());
 });
