1381b3ca1da7f1f4f897a9ccef20501ced9163d9
bizen241 <bizen241@gmail.com>
2018-11-24 22:12:05 +0900

    chore(api): use error boundary

--- src/server/routes/api/user.ts
+++ src/server/routes/api/user.ts
@@ -1,10 +1,10 @@
 import { OperationFunction } from "express-openapi";
-import { createApiDoc } from "../../api/operation";
+import { createApiDoc, errorBoundary } from "../../api/operation";
 import { responseFindResult } from "../../api/response";
 
-export const GET: OperationFunction = async (req, res) => {
+export const GET: OperationFunction = errorBoundary(async (req, res) => {
   responseFindResult(res, req.session.user);
-};
+});
 
 GET.apiDoc = createApiDoc({
   summary: "Get the authenticated user",
