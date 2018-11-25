dd5037ac6780ba83d0418a0f3be4e352d3c68dea
bizen241 <bizen241@gmail.com>
2018-11-25 22:04:41 +0900

    chore(api): update operation document creator

--- src/server/api/operation.ts
+++ src/server/api/operation.ts
@@ -2,14 +2,16 @@ import { NextFunction } from "connect";
 import { Request, RequestHandler, Response } from "express";
 import * as createError from "http-errors";
 import { OpenAPIV2 } from "openapi-types";
-import { Permission } from "../../shared/api/entities";
+import { EntityObject, Permission } from "../../shared/api/entities";
 
 type Tag = "user" | "users" | "user-accounts" | "user-sessions";
 
-interface Parameters {
+interface OperationDocument<E extends EntityObject> {
   summary: string;
   tag: Tag;
   permission: Permission;
+  body?: { [P in keyof E]?: null };
+  query?: { [P in keyof E]?: null };
 }
 
 const responses: OpenAPIV2.ResponsesObject = {
@@ -27,16 +29,32 @@ const responses: OpenAPIV2.ResponsesObject = {
   }
 };
 
-export const createApiDoc = ({ summary, tag, permission }: Parameters): OpenAPIV2.OperationObject => ({
-  summary,
-  tags: [tag],
-  responses,
-  security: [
-    {
-      [permission]: []
-    }
-  ]
-});
+const createOperationParameters = (location: "body" | "query", source: object = {}): OpenAPIV2.Parameters =>
+  Object.keys(source).map(name => ({
+    in: location,
+    type: "string",
+    name
+  }));
+
+export const createOperationDoc = <E extends EntityObject>(
+  document: OperationDocument<E>
+): OpenAPIV2.OperationObject => {
+  const { summary, tag, permission, body = {}, query = {} } = document;
+
+  const parameters = [...createOperationParameters("body", body), ...createOperationParameters("query", query)];
+
+  return {
+    summary,
+    tags: [tag],
+    responses,
+    parameters,
+    security: [
+      {
+        [permission]: []
+      }
+    ]
+  };
+};
 
 type AsyncRequestHandler = (req: Request, res: Response, next: NextFunction) => Promise<any>;
 
