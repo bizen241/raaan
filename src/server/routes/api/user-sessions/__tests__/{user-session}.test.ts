f14603e3a87f0824b89b1d23ab2bc59864aa05cc
bizen241 <bizen241@gmail.com>
2018-11-25 11:54:06 +0900

    chore(api): add GET /user-sessions/{user-session}

---
+++ src/server/routes/api/user-sessions/__tests__/{user-session}.test.ts
@@ -0,0 +1,39 @@
+import { getManager } from "typeorm";
+import { EntityStore } from "../../../../../shared/api/response/entity";
+import { TestDatabase } from "../../../../database/__tests__/helpers";
+import { sessions, users } from "../../../../session/__tests__/helpers";
+import { createHttpMocks } from "../../__tests__/helpers";
+import { GET, PathParams } from "../{user-session}";
+
+const testDatabase = new TestDatabase();
+
+beforeAll(async () => {
+  await testDatabase.connect();
+});
+afterAll(async () => {
+  await testDatabase.close();
+});
+
+beforeEach(async () => {
+  await testDatabase.reset();
+});
+
+test("GET /api/user-sessions/{user-session}", async () => {
+  await getManager().save(users.Read);
+  await getManager().save(sessions.Read);
+
+  const { req, res } = createHttpMocks("Read");
+
+  const params: PathParams = {
+    "user-session": sessions.Read.id
+  };
+
+  req.params = params;
+
+  await GET(req, res, () => null);
+
+  expect(res.statusCode).toEqual(200);
+
+  const data = JSON.parse(res._getData()) as EntityStore;
+  expect(data.User[users.Read.id]).toBeDefined();
+});
---
+++ src/server/routes/api/user-sessions/{user-session}.ts
@@ -0,0 +1,36 @@
+import { OperationFunction } from "express-openapi";
+import * as createError from "http-errors";
+import { getManager } from "typeorm";
+import { createApiDoc, errorBoundary } from "../../../api/operation";
+import { responseFindResult } from "../../../api/response";
+import { UserSessionEntity } from "../../../database/entities";
+
+export interface PathParams {
+  "user-session": string;
+}
+
+export const GET: OperationFunction = errorBoundary(async (req, res, next) => {
+  const currentUser = req.session.user;
+
+  const { "user-session": userSessionId }: PathParams = req.params;
+
+  const result = await getManager().findOne(UserSessionEntity, userSessionId, {
+    relations: ["user"]
+  });
+  if (result === undefined) {
+    return next(createError(404));
+  }
+  if (result.user.id !== currentUser.id && currentUser.permission !== "Admin") {
+    next(createError(403));
+
+    return;
+  }
+
+  responseFindResult(res, result);
+});
+
+GET.apiDoc = createApiDoc({
+  summary: "Get an user session",
+  tag: "user-sessions",
+  permission: "Read"
+});
