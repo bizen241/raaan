c5d9691640d7ebc5f9c175857ef32c2fb2372b13
bizen241 <bizen241@gmail.com>
2018-12-06 14:05:17 +0900

    chore(db): update Entity

--- src/server/database/entities/index.ts
+++ src/server/database/entities/index.ts
@@ -1,11 +1,30 @@
+import { ContentBranchEntity } from "./ContentBranchEntity";
+import { ContentEntity } from "./ContentEntity";
+import { ContentRevisionEntity } from "./ContentRevisionEntity";
 import { UserAccountEntity } from "./UserAccountEntity";
 import { UserEntity } from "./UserEntity";
 import { UserSessionEntity } from "./UserSessionEntity";
 
+export * from "./ContentEntity";
+export * from "./ContentBranchEntity";
+export * from "./ContentRevisionEntity";
 export * from "./UserAccountEntity";
 export * from "./UserEntity";
 export * from "./UserSessionEntity";
 
-export type Entity = UserAccountEntity | UserSessionEntity | UserEntity;
+export type Entity =
+  | ContentEntity
+  | ContentBranchEntity
+  | ContentRevisionEntity
+  | UserAccountEntity
+  | UserSessionEntity
+  | UserEntity;
 
-export const entities = [UserAccountEntity, UserSessionEntity, UserEntity];
+export const entities = [
+  ContentEntity,
+  ContentBranchEntity,
+  ContentRevisionEntity,
+  UserAccountEntity,
+  UserSessionEntity,
+  UserEntity
+];
