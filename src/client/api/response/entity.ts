05b8bfc35ca590118dd0169a3260a27ee98fa764
bizen241 <bizen241@gmail.com>
2018-12-06 14:06:59 +0900

    chore(api): add Content to EntityStore

--- src/client/api/response/entity.ts
+++ src/client/api/response/entity.ts
@@ -1,6 +1,9 @@
 import { EntityStore } from "../../../shared/api/response/entity";
 
 export const mergeEntityStore = (target: EntityStore, source: EntityStore): EntityStore => ({
+  Content: { ...target.Content, ...source.Content },
+  ContentBranch: { ...target.ContentBranch, ...source.ContentBranch },
+  ContentRevision: { ...target.ContentRevision, ...source.ContentRevision },
   User: { ...target.User, ...source.User },
   UserAccount: { ...target.UserAccount, ...source.UserAccount },
   UserSession: { ...target.UserSession, ...source.UserSession }
--- src/shared/api/response/entity.ts
+++ src/shared/api/response/entity.ts
@@ -1,16 +1,22 @@
-import { EntityObject, User, UserAccount, UserSession } from "../entities";
+import { Content, ContentBranch, ContentRevision, EntityObject, User, UserAccount, UserSession } from "../entities";
 
 export interface EntityMap<E extends EntityObject> {
   [id: string]: E | undefined;
 }
 
 export interface EntityStore {
+  Content: EntityMap<Content>;
+  ContentBranch: EntityMap<ContentBranch>;
+  ContentRevision: EntityMap<ContentRevision>;
   User: EntityMap<User>;
   UserAccount: EntityMap<UserAccount>;
   UserSession: EntityMap<UserSession>;
 }
 
 export const createEntityStore = (): EntityStore => ({
+  Content: {},
+  ContentBranch: {},
+  ContentRevision: {},
   User: {},
   UserAccount: {},
   UserSession: {}
