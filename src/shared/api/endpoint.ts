be16ffa739ed65c2c1d82d10e41eb88839623fbd
bizen241 <bizen241@gmail.com>
2018-12-06 14:08:01 +0900

    chore(api): update endpoints

--- src/shared/api/endpoint.ts
+++ src/shared/api/endpoint.ts
@@ -1,6 +1,9 @@
 import { EntityType } from "./entities";
 
 export const endpoints: { [P in EntityType]: string } = {
+  Content: "contents",
+  ContentBranch: "content-branches",
+  ContentRevision: "content-revisions",
   User: "users",
   UserAccount: "user-accounts",
   UserSession: "user-sessions"
