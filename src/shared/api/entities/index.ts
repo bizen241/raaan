da585f3dbf34eb2135db9841a9288a2811a6c59d
bizen241 <bizen241@gmail.com>
2018-12-06 14:05:39 +0900

    chore(api): update EntityObject

--- src/shared/api/entities/index.ts
+++ src/shared/api/entities/index.ts
@@ -1,11 +1,17 @@
+import { Content } from "./Content";
+import { ContentBranch } from "./ContentBranch";
+import { ContentRevision } from "./ContentRevision";
 import { User } from "./User";
 import { UserAccount } from "./UserAccount";
 import { UserSession } from "./UserSession";
 
+export * from "./Content";
+export * from "./ContentBranch";
+export * from "./ContentRevision";
 export * from "./User";
 export * from "./UserAccount";
 export * from "./UserSession";
 
 export type EntityType = "Content" | "ContentBranch" | "ContentRevision" | "User" | "UserAccount" | "UserSession";
 
-export type EntityObject = User | UserAccount | UserSession;
+export type EntityObject = Content | ContentBranch | ContentRevision | User | UserAccount | UserSession;
