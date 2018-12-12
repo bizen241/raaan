36b905f8a1b64f645459cdda2c6bafbb5ea951b3
bizen241 <bizen241@gmail.com>
2018-12-13 08:55:13 +0900

    chore(api): add /contents

---
+++ src/server/routes/api/contents.ts
@@ -0,0 +1,24 @@
+import { OperationFunction } from "express-openapi";
+import { getManager } from "typeorm";
+import { Content } from "../../../shared/api/entities";
+import { createOperationDoc, errorBoundary } from "../../api/operation";
+import { parseSearchParams } from "../../api/request/search";
+import { responseSearchResult, skip, take } from "../../api/response";
+import { ContentEntity } from "../../database/entities";
+
+export const GET: OperationFunction = errorBoundary(async (req, res) => {
+  const { page } = parseSearchParams<Content>("Content", req.query);
+
+  const result = await getManager().findAndCount(ContentEntity, {
+    skip: skip(page),
+    take
+  });
+
+  responseSearchResult(res, ...result);
+});
+
+GET.apiDoc = createOperationDoc<Content>({
+  summary: "Search contents",
+  tag: "contents",
+  permission: "Guest"
+});
