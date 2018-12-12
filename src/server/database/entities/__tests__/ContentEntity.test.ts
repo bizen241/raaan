cfe9fb935866134152a7be1f4f10924b22ac2b97
bizen241 <bizen241@gmail.com>
2018-12-12 14:58:43 +0900

    test(db): add helper for ContentEntity

--- src/server/database/entities/__tests__/ContentEntity.test.ts
+++ src/server/database/entities/__tests__/ContentEntity.test.ts
@@ -1,10 +1,7 @@
 import { getManager } from "typeorm";
-import * as uuid from "uuid";
-import { users } from "../../../session/__tests__/helpers";
 import { TestDatabase } from "../../__tests__/helpers";
-import { createContentBranch } from "../ContentBranchEntity";
-import { ContentEntity, createContent } from "../ContentEntity";
-import { createContentRevision } from "../ContentRevisionEntity";
+import { ContentEntity } from "../ContentEntity";
+import { insertContent } from "./helpers";
 
 const testDatabase = new TestDatabase();
 
@@ -16,39 +13,9 @@ afterAll(async () => {
 });
 
 test("ContentEntity", async () => {
-  const manager = getManager();
+  await insertContent();
 
-  await manager.save(users.Read);
-
-  const content = createContent({
-    id: uuid()
-  });
-  const branch = createContentBranch({
-    id: uuid(),
-    content,
-    lang: "ja"
-  });
-  const revision = createContentRevision({
-    id: uuid(),
-    branch,
-    author: users.Read,
-    version: 1,
-    comment: "",
-    object: {},
-    isDraft: true
-  });
-
-  await manager.save(content);
-  await manager.save(branch);
-  await manager.save(revision);
-
-  content.source = branch;
-  branch.latest = revision;
-
-  await manager.save(content);
-  await manager.save(branch);
-
-  const contents = await manager.find(ContentEntity);
+  const contents = await getManager().find(ContentEntity);
 
   expect(contents.length).toBe(1);
 });
---
+++ src/server/database/entities/__tests__/helpers.ts
@@ -0,0 +1,34 @@
+import { getManager } from "typeorm";
+import * as uuid from "uuid";
+import { users } from "../../../session/__tests__/helpers";
+import { createContentBranch } from "../ContentBranchEntity";
+import { createContent } from "../ContentEntity";
+import { createContentRevision } from "../ContentRevisionEntity";
+
+export const insertContent = async (contentId?: string) => {
+  const manager = getManager();
+
+  await manager.save(users.Read);
+
+  const content = createContent({ id: uuid() });
+  const branch = createContentBranch({ id: uuid(), content, lang: "ja" });
+  const revision = createContentRevision({
+    id: contentId || uuid(),
+    branch,
+    author: users.Read,
+    version: 1,
+    comment: "",
+    object: {},
+    isDraft: true
+  });
+
+  await manager.save(content);
+  await manager.save(branch);
+  await manager.save(revision);
+
+  content.source = branch;
+  branch.latest = revision;
+
+  await manager.save(content);
+  await manager.save(branch);
+};
