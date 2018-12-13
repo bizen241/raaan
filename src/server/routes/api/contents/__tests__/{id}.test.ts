18ea1aeb372bbe1fd15b11416a73410190fd0798
bizen241 <bizen241@gmail.com>
2018-12-13 09:28:52 +0900

    chore(api): add /contents/{id}

---
+++ src/server/routes/api/contents/__tests__/{id}.test.ts
@@ -0,0 +1,89 @@
+import { getManager } from "typeorm";
+import * as uuid from "uuid";
+import { EntityStore } from "../../../../../shared/api/response/entity";
+import { PathParams } from "../../../../api/operation";
+import { TestDatabase } from "../../../../database/__tests__/helpers";
+import { ContentEntity } from "../../../../database/entities";
+import { insertContent } from "../../../../database/entities/__tests__/helpers";
+import { insertSessions, insertUsers } from "../../../../session/__tests__/helpers";
+import { createHttpMocks } from "../../__tests__/helpers";
+import { GET } from "../{id}";
+
+const testDatabase = new TestDatabase();
+
+beforeAll(async () => {
+  await testDatabase.connect();
+});
+afterAll(async () => {
+  await testDatabase.close();
+});
+
+beforeEach(async () => {
+  await testDatabase.reset();
+
+  await insertUsers();
+  await insertSessions();
+});
+
+test("GET /api/contents/{id} -> 404", async () => {
+  const { req, res, next } = createHttpMocks("Guest");
+
+  (req.params as PathParams) = {
+    id: uuid()
+  };
+
+  await GET(req, res, next);
+
+  expect(res._getStatusCode()).toEqual(404);
+});
+
+test("GET /api/contents/{id} -> 200", async () => {
+  const { req, res, next } = createHttpMocks("Guest");
+
+  const contentId = uuid();
+  await insertContent(contentId);
+
+  (req.params as PathParams) = {
+    id: contentId
+  };
+
+  await GET(req, res, next);
+
+  expect(res._getStatusCode()).toEqual(200);
+
+  const data = JSON.parse(res._getData()) as EntityStore;
+  expect(data.Content[contentId]).toBeDefined();
+});
+
+test("DELETE /api/contents/{id} -> 404", async () => {
+  const { req, res, next } = createHttpMocks("Admin");
+
+  (req.params as PathParams) = {
+    id: uuid()
+  };
+
+  await GET(req, res, next);
+
+  expect(res._getStatusCode()).toEqual(404);
+});
+
+test("GET /api/contents/{id} -> 200", async () => {
+  const { req, res, next } = createHttpMocks("Admin");
+
+  const contentId = uuid();
+  await insertContent(contentId);
+
+  (req.params as PathParams) = {
+    id: contentId
+  };
+
+  await GET(req, res, next);
+
+  expect(res._getStatusCode()).toEqual(200);
+
+  const data = JSON.parse(res._getData()) as EntityStore;
+  expect(data.Content[contentId]).toBeDefined();
+
+  const removedContent = await getManager().findOne(ContentEntity, contentId);
+  expect(removedContent).toBeUndefined();
+});
---
+++ src/server/routes/api/contents/{id}.ts
@@ -0,0 +1,46 @@
+import { OperationFunction } from "express-openapi";
+import * as createError from "http-errors";
+import { getManager } from "typeorm";
+import { createOperationDoc, errorBoundary, PathParams } from "../../../api/operation";
+import { responseFindResult } from "../../../api/response";
+import { ContentEntity } from "../../../database/entities";
+
+export const GET: OperationFunction = errorBoundary(async (req, res, next) => {
+  const { id: contentId }: PathParams = req.params;
+
+  const loadedContent = await getManager().findOne(ContentEntity, contentId);
+  if (loadedContent === undefined) {
+    return next(createError(404));
+  }
+
+  responseFindResult(res, loadedContent);
+});
+
+GET.apiDoc = createOperationDoc({
+  summary: "Get a content",
+  tag: "contents",
+  permission: "Guest",
+  path: ["id"]
+});
+
+export const DELETE: OperationFunction = errorBoundary(async (req, res, next) => {
+  const { id: contentId }: PathParams = req.params;
+
+  const manager = getManager();
+
+  const loadedContent = await manager.findOne(ContentEntity, contentId);
+  if (loadedContent === undefined) {
+    return next(createError(404));
+  }
+
+  await manager.remove(loadedContent);
+
+  responseFindResult(res, loadedContent);
+});
+
+DELETE.apiDoc = createOperationDoc({
+  summary: "Delete a content",
+  tag: "contents",
+  permission: "Admin",
+  path: ["id"]
+});
