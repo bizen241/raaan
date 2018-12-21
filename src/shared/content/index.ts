5207715f1f76a8ac11082a01c9ed4e1ce0fe0eee
bizen241 <bizen241@gmail.com>
2018-12-21 15:35:32 +0900

    chore(content): define content parts

--- src/shared/content/index.ts
+++ src/shared/content/index.ts
@@ -1,11 +1,32 @@
-export interface Sentence {
+interface Part {
   id: string;
   value: string;
+  comment: string;
+}
+
+export interface TextPart extends Part {
+  type: "text";
+  lang: string;
   display: string;
 }
 
+export interface CodePart extends Part {
+  type: "code";
+  lang: string;
+}
+
+export interface MathPart extends Part {
+  type: "math";
+}
+
+export type ContentPart = TextPart | CodePart | MathPart;
+
 export interface ContentData {
+  version: 1;
   title: string;
-  description: string;
-  sentences: Sentence[];
+  lang: string;
+  tags: string[];
+  parts: ContentPart[];
+  comment: string;
+  shuffle: boolean;
 }
