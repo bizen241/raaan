7f8cb25b0b7ae9dd97c45889d6c0034240443fd5
bizen241 <bizen241@gmail.com>
2018-12-22 15:55:31 +0900

    chore(flux): introduce redux-devtools-extension

--- src/client/store/index.ts
+++ src/client/store/index.ts
@@ -1,4 +1,9 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import { rootReducer } from "../reducers";
 
-export const configureStore = () => createStore(rootReducer);
+const isDevelopment = process.env.NODE_ENV === "development" || location.hostname === "localhost";
+const storeEnhancer = isDevelopment ? composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk);
+
+export const configureStore = () => createStore(rootReducer, storeEnhancer);
