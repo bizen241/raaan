a05bb2cc67e58d1eb4a09e25512969f610833171
bizen241 <bizen241@gmail.com>
2018-12-24 20:52:42 +0900

    chore(flux): introduce redux-persist

--- src/client/store/index.ts
+++ src/client/store/index.ts
@@ -1,9 +1,23 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { PersistConfig, persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import { rootReducer } from "../reducers";
 
 const isDevelopment = process.env.NODE_ENV === "development" || location.hostname === "localhost";
 const storeEnhancer = isDevelopment ? composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk);
 
-export const configureStore = () => createStore(rootReducer, storeEnhancer);
+const persistConfig: PersistConfig = {
+  key: "root",
+  version: 0,
+  storage
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => {
+  const store = createStore(persistedReducer, storeEnhancer);
+  const persistor = persistStore(store);
+
+  return { store, persistor };
+};
