b21461d60f8ba6b9b587230f367213399df4c2c0
bizen241 <bizen241@gmail.com>
2018-12-31 16:17:51 +0900

    chore(client): implement Edit page

---
+++ src/client/components/editor/index.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import { FunctionComponent } from "react";
+import { ContentItem, TextItem } from "../../../shared/content";
+
+export const ContentItemEditor: FunctionComponent<{
+  index: number;
+  item: ContentItem;
+  onUpdate: (index: number, item: ContentItem) => void;
+  onDelete: (index: number) => void;
+}> = ({ index, item, onUpdate, onDelete }) => {
+  const Editor = editors[item.type];
+
+  return (
+    <div>
+      <Editor item={item} onChange={(textItem: TextItem) => onUpdate(index, textItem)} />
+      <button onClick={() => onDelete(index)}>プレビュー</button>
+      <button onClick={() => onDelete(index)}>削除</button>
+    </div>
+  );
+};
+
+interface ContentItemEditorProps<T> {
+  item: T;
+  onChange: (item: T) => void;
+}
+
+const TextItemEditor: FunctionComponent<ContentItemEditorProps<TextItem>> = ({ item, onChange }) => {
+  return (
+    <div>
+      <label>
+        表示
+        <textarea value={item.display} onChange={e => onChange({ ...item, display: e.currentTarget.value })} />
+      </label>
+      <label>
+        入力
+        <textarea value={item.value} onChange={e => onChange({ ...item, value: e.currentTarget.value })} />
+      </label>
+    </div>
+  );
+};
+
+const CodeItemEditor: FunctionComponent<ContentItemEditorProps<TextItem>> = ({ item, onChange }) => {
+  return (
+    <div>
+      <textarea value={item.value} onChange={e => onChange({ ...item, value: e.currentTarget.value })} />
+    </div>
+  );
+};
+
+const MathItemEditor: FunctionComponent<ContentItemEditorProps<TextItem>> = ({ item, onChange }) => {
+  return (
+    <div>
+      <textarea value={item.value} onChange={e => onChange({ ...item, value: e.currentTarget.value })} />
+    </div>
+  );
+};
+
+const editors: { [T in ContentItem["type"]]: FunctionComponent<ContentItemEditorProps<any>> } = {
+  text: TextItemEditor,
+  code: CodeItemEditor,
+  math: MathItemEditor
+};
--- src/client/components/pages/Edit.tsx
+++ src/client/components/pages/Edit.tsx
@@ -1,13 +1,38 @@
 import * as React from "react";
+import { useEffect } from "react";
+import { createTextItem } from "../../domain/content";
 import { connector } from "../../reducers";
+import { editorActions } from "../../reducers/editor";
+import { ContentItemEditor } from "../editor";
 import { PageProps } from "../project/Router";
 
 export const Edit = connector(
-  (_, ownProps: PageProps) => ({
-    id: ownProps.match.params.id
+  (state, ownProps: PageProps) => ({
+    id: ownProps.match.params.id,
+    data: state.editor.data
   }),
-  () => ({}),
-  ({ id }) => {
-    return <div>Edit {id}</div>;
+  () => ({
+    ...editorActions
+  }),
+  ({ id, data, load, updateTitle, addItem, updateItem, deleteItem }) => {
+    useEffect(() => {
+      load(id);
+    }, []);
+
+    return (
+      <div>
+        <div>
+          <label>
+            タイトル
+            <input value={data.title} onChange={e => updateTitle(e.currentTarget.value)} />
+          </label>
+        </div>
+        <label>アイテム</label>
+        {data.items.map((item, index) => (
+          <ContentItemEditor key={item.id} index={index} item={item} onUpdate={updateItem} onDelete={deleteItem} />
+        ))}
+        <button onClick={() => addItem(data.items.length, createTextItem())}>追加</button>
+      </div>
+    );
   }
 );
