ef7b4258340b760d510dfc93746f745c15793ac4
bizen241 <bizen241@gmail.com>
2019-01-10 23:18:05 +0900

    chore(content): add KanjiItem

--- src/shared/content/index.ts
+++ src/shared/content/index.ts
@@ -1,25 +1,32 @@
 interface Item {
   id: string;
-  value: string;
   comment: string;
 }
 
 export interface TextItem extends Item {
   type: "text";
   lang: string;
-  display: string;
+  text: string;
+}
+
+export interface KanjiItem extends Item {
+  type: "kanji";
+  kanji: string;
+  kana: string;
 }
 
 export interface CodeItem extends Item {
   type: "code";
   lang: string;
+  code: string;
 }
 
 export interface MathItem extends Item {
   type: "math";
+  formula: string;
 }
 
-export type ContentItem = TextItem | CodeItem | MathItem;
+export type ContentItem = TextItem | KanjiItem | CodeItem | MathItem;
 
 export interface ContentData {
   version: 1;
