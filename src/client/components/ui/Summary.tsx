2149042013ef67cc6dff8314d34ebeb37197e5d3
bizen241 <bizen241@gmail.com>
2019-01-13 09:41:49 +0900

    chore(style): customize Summary

--- src/client/components/ui/Summary.tsx
+++ src/client/components/ui/Summary.tsx
@@ -4,8 +4,19 @@ import { fontSizes, paddings, Size } from "./values";
 export const Summary = styled.summary<{
   size?: Size;
 }>`
-  padding: ${p => paddings[p.size || "medium"]};
+  display: flex;
+  align-items: center;
+  padding: 0 ${p => paddings[p.size || "medium"]};
   color: ${p => p.theme.text};
   font-family: inherit;
   font-size: ${p => fontSizes[p.size || "medium"]};
+
+  ::before {
+    content: "▶";
+    font-size: 0.8em;
+  }
+
+  details[open] &::before {
+    content: "▼";
+  }
 `;
