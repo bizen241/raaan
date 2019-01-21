8adc2bbf5cd2787a1c2810fb2f9a8f8cd9e0e6ab
bizen241 <bizen241@gmail.com>
2019-01-21 20:56:45 +0900

    chore(client): import css files

--- src/client/render.tsx
+++ src/client/render.tsx
@@ -3,6 +3,9 @@ import * as ReactDOM from "react-dom";
 import * as ReactModal from "react-modal";
 import { App } from "./components/project/App";
 
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
 export const renderApp = () => {
   const rootElement = document.getElementById("root")!;
 
