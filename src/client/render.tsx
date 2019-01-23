b510490acf76af3a37dbee11ff826451c8c22fe4
bizen241 <bizen241@gmail.com>
2019-01-23 15:39:21 +0900

    fix(client): delete react-modal

--- src/client/render.tsx
+++ src/client/render.tsx
@@ -1,14 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import * as ReactModal from "react-modal";
 import { App } from "./components/project/App";
 
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
 export const renderApp = () => {
-  const rootElement = document.getElementById("root")!;
-
-  ReactModal.setAppElement(rootElement);
-  ReactDOM.render(<App />, rootElement);
+  ReactDOM.render(<App />, document.getElementById("root"));
 };
