0ddfb0f808f02b3e83402bb0e33f72a5da4b032d
bizen241 <bizen241@gmail.com>
2019-01-23 15:16:01 +0900

    chore(ui): delete unused components

--- src/client/components/ui/Details.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from "../../style";
-
-export const Details = styled.details`
-  background-color: ${p => p.theme.container};
-  border-style: solid;
-  border-width: 1px;
-  border-color: ${p => p.theme.border};
-  border-radius: 2px;
-`;
