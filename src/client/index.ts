77651ff54c50c0ab24d45166a8c9f30a3fda015e
bizen241 <bizen241@gmail.com>
2019-01-23 19:08:00 +0900

    fix(client): move import statements

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,6 @@
 import { renderApp } from "./render";
 
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
 renderApp();
--- src/client/render.tsx
+++ src/client/render.tsx
@@ -2,9 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./components/project/App";
 
-import "@blueprintjs/core/lib/css/blueprint.css";
-import "@blueprintjs/icons/lib/css/blueprint-icons.css";
-
 export const renderApp = () => {
   ReactDOM.render(<App />, document.getElementById("root"));
 };
