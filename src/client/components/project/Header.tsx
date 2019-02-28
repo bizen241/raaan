a725debdea4fc3bfa0a33ba71d57d28c518804eb
bizen241 <bizen241@gmail.com>
2019-02-28 16:03:35 +0900

    chore(ui): modify header buttons

--- src/client/components/project/Header.tsx
+++ src/client/components/project/Header.tsx
@@ -1,26 +1,40 @@
-import { Button, Classes, Navbar, NavbarGroup } from "@blueprintjs/core";
-import { goBack } from "connected-react-router";
+import { Button, ButtonGroup, Classes, Navbar, NavbarGroup } from "@blueprintjs/core";
+import { goBack, goForward } from "connected-react-router";
 import * as React from "react";
+import { Link } from "react-router-dom";
 import { connector } from "../../reducers";
 import { Row } from "../ui";
 
 export const Header = connector(
   (state, ownProps: { heading: string }) => ({
     heading: ownProps.heading,
-    canBack: state.router.location.pathname !== "/"
+    pathname: state.router.location.pathname
   }),
   () => ({
-    back: goBack
+    back: goBack,
+    forward: goForward
   }),
-  ({ heading, canBack, back }) => {
+  ({ heading, pathname, back, forward }) => {
+    const isHome = pathname === "/";
+
     return (
       <Navbar>
         <NavbarGroup align="center">
           <Row padding>
-            <Button icon="arrow-left" disabled={!canBack} onClick={back} />
+            <ButtonGroup>
+              <Button icon="arrow-left" onClick={back} />
+              <Button icon="arrow-right" onClick={forward} />
+            </ButtonGroup>
           </Row>
           <Row padding flex={1}>
             <div className={Classes.TEXT_OVERFLOW_ELLIPSIS}>{heading}</div>
+          </Row>
+          <Row padding>
+            {isHome ? (
+              <Button icon="home" disabled />
+            ) : (
+              <Link className={`${Classes.BUTTON} ${Classes.iconClass("home")}`} to="/" />
+            )}
           </Row>
         </NavbarGroup>
       </Navbar>
