58a04a6b7f7b25140e33f9ff7409b11b2ac086db
bizen241 <bizen241@gmail.com>
2019-03-27 14:33:41 +0900

    chore(client): get browser locale

--- src/client/components/project/Translator.tsx
+++ src/client/components/project/Translator.tsx
@@ -1,12 +1,19 @@
 import { I18nProvider } from "@lingui/react";
 import * as React from "react";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
+import { Lang } from "../../../shared/api/entities";
 import { catalogs } from "../../intl";
 import { ConfigContext } from "./Initializer";
 
 export const Translator = React.memo<{ children: React.ReactNode }>(({ children }) => {
-  const config = useContext(ConfigContext);
-  const language = config.lang || "en";
+  const { lang } = useContext(ConfigContext);
+  const language = useMemo<Lang>(() => {
+    if (lang === "default" || lang === "system" || lang === undefined) {
+      return navigator.language.slice(0, 2) as Lang;
+    } else {
+      return lang;
+    }
+  }, [lang]);
 
   return (
     <I18nProvider language={language} catalogs={catalogs}>
