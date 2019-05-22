bfabf687a233e7901e007de31aff577dc2228571
bizen241 <bizen241@gmail.com>
2019-05-23 08:10:09 +0900

    fix(api): fix query

--- src/server/routes/api/exercise-summaries.ts
+++ src/server/routes/api/exercise-summaries.ts
@@ -4,21 +4,21 @@ import { ExerciseSummary } from "../../../shared/api/entities";
 import { createOperationDoc, errorBoundary } from "../../api/operation";
 import { parseSearchParams } from "../../api/request/search/parse";
 import { responseSearchResult } from "../../api/response";
-import { ExerciseEntity } from "../../database/entities";
+import { ExerciseSummaryEntity } from "../../database/entities";
 
 export const GET: OperationFunction = errorBoundary(async (req, res) => {
   const { tags, limit, offset } = parseSearchParams<ExerciseSummary>("ExerciseSummary", req.query);
 
   const query = await getManager()
-    .createQueryBuilder(ExerciseEntity, "exercise")
-    .leftJoinAndSelect("exercise.author", "author")
-    .leftJoinAndSelect("exercise.detail", "detail")
-    .leftJoinAndSelect("exercise.tags", "tags")
+    .createQueryBuilder(ExerciseSummaryEntity, "exerciseSummary")
+    .leftJoinAndSelect("exerciseSummary.exercise", "exercise")
+    .leftJoinAndSelect("exerciseSummary.tags", "tags")
+    .leftJoinAndSelect("exercise.author", "exercise.author")
     .take(limit)
     .skip(offset);
 
   if (tags !== undefined) {
-    query.innerJoin("exercise.tags", "tags", "tags.name IN (:...tags)", { tags });
+    query.innerJoin("exerciseSummary.tags", "tags", "tags.name IN (:...tags)", { tags });
   }
 
   const result = await query.getManyAndCount();
