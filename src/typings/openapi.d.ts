52b77ee8a25107cb55bd6d6b2d449b9d89c78934
bizen241 <bizen241@gmail.com>
2019-06-02 07:55:41 +0900

    chore(types): delete unused type definitions

--- src/typings/openapi.d.ts
+++ src/typings/openapi.d.ts
@@ -1,9 +1,9 @@
-import { UserSessionEntity, UserEntity } from "../server/database/entities";
+import { UserEntity } from "../server/database/entities";
 
 declare module "openapi-types" {
   namespace OpenAPI {
     export interface Request {
-      user: UserEntity | undefined;
+      user?: UserEntity;
     }
   }
 }
