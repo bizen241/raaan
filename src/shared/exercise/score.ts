1b1811b0c6e1ec7fe2eae5908bdef5d4e4bcef26
bizen241 <bizen241@gmail.com>
2019-06-16 09:08:38 +0900

    chore(attempt): update score

--- src/shared/exercise/score.ts
+++ src/shared/exercise/score.ts
@@ -1,4 +1,4 @@
 import { AttemptResult } from "../api/entities";
 
 export const calculateScore = ({ keystrokes, time, accuracy }: AttemptResult) =>
-  Math.floor((keystrokes / (time / 1000)) * accuracy * 100);
+  Math.floor((keystrokes / (time / 1000)) * 60 * accuracy);
