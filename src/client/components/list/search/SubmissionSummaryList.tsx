5d8dfed11aadf8be183bd489c3787369722c40b7
bizen241 <bizen241@gmail.com>
2019-06-27 15:17:52 +0900

    chore(list): delete session

--- src/client/components/list/search/SubmissionSummaryList.tsx
+++ src/client/components/list/search/SubmissionSummaryList.tsx
@@ -1,11 +1,8 @@
-import { Box, IconButton, TableCell } from "@material-ui/core";
-import { Delete } from "@material-ui/icons";
+import { Box, TableCell } from "@material-ui/core";
 import * as React from "react";
-import { useContext } from "react";
 import { EntityList, EntityListItemProps, EntityListProps } from ".";
 import { ExerciseSummary, SubmissionSummary } from "../../../../shared/api/entities";
 import { useEntity } from "../../../hooks/search";
-import { UserContext } from "../../project/Context";
 
 export const SubmissionSummaryList = React.memo<EntityListProps<SubmissionSummary>>(props => {
   return <EntityList {...props} entityType="SubmissionSummary" itemComponent={SubmissionSummaryListItem} />;
@@ -13,8 +10,6 @@ export const SubmissionSummaryList = React.memo<EntityListProps<SubmissionSummar
 
 const SubmissionSummaryListItem = React.memo<EntityListItemProps<SubmissionSummary>>(
   ({ entity: submissionSummary }) => {
-    const currentUser = useContext(UserContext);
-
     const { entity: exerciseSummary } = useEntity<ExerciseSummary>(
       "ExerciseSummary",
       submissionSummary.exerciseSummaryId
@@ -28,13 +23,6 @@ const SubmissionSummaryListItem = React.memo<EntityListItemProps<SubmissionSumma
             {title}
           </Box>
         </TableCell>
-        {submissionSummary.userId === currentUser.id ? (
-          <TableCell padding="checkbox">
-            <IconButton>
-              <Delete />
-            </IconButton>
-          </TableCell>
-        ) : null}
       </>
     );
   }
--- src/client/components/list/search/UserSessionList.tsx
+++ src/client/components/list/search/UserSessionList.tsx
@@ -1,9 +1,11 @@
 import { Box, IconButton, TableCell } from "@material-ui/core";
 import { Delete } from "@material-ui/icons";
 import * as React from "react";
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
+import { useDispatch } from "react-redux";
 import { EntityList, EntityListItemProps, EntityListProps } from ".";
 import { UserSession } from "../../../../shared/api/entities";
+import { actions } from "../../../reducers";
 import { UserContext } from "../../project/Context";
 
 export const UserSessionList = React.memo<EntityListProps<UserSession>>(props => {
@@ -11,6 +13,7 @@ export const UserSessionList = React.memo<EntityListProps<UserSession>>(props =>
 });
 
 const UserSessionListItem = React.memo<EntityListItemProps<UserSession>>(({ entity: userSession }) => {
+  const dispatch = useDispatch();
   const currentUser = useContext(UserContext);
 
   return (
@@ -22,7 +25,7 @@ const UserSessionListItem = React.memo<EntityListItemProps<UserSession>>(({ enti
       </TableCell>
       {userSession.userId === currentUser.id ? (
         <TableCell padding="checkbox">
-          <IconButton>
+          <IconButton onClick={useCallback(() => dispatch(actions.api.delete("UserSession", userSession.id)), [])}>
             <Delete />
           </IconButton>
         </TableCell>
