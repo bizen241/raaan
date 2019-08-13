2e49ec30986820552b30544b4a35c7389d74acd7
bizen241 <bizen241@gmail.com>
2019-08-14 01:38:34 +0900

    chore(server): get env

--- src/server/env.ts
+++ src/server/env.ts
@@ -16,55 +16,43 @@ export interface ProcessEnv {
 }
 
 export const getProcessEnv = (): ProcessEnv => {
-  const {
-    HOST,
-    PORT,
-    DATABASE_URL,
-    SESSION_SECRET,
-    OWNER_ACCOUNT_PROVIDER,
-    OWNER_ACCOUNT_ID,
-    OWNER_ACCOUNT_NAME,
-    OWNER_ACCOUNT_EMAIL,
-    GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET
-  } = process.env;
+  const serverHost = getEnv("SERVER_HOST");
+  const serverPort = Number(getEnv("SERVER_PORT"));
+  const databaseUrl = getEnv("DATABASE_URL");
+  const sessionSecret = getEnv("SESSION_SECRET");
+  const ownerAccountProvider = getEnv("OWNER_ACCOUNT_PROVIDER");
+  const ownerAccountId = getEnv("OWNER_ACCOUNT_ID");
+  const ownerAccountName = getEnv("OWNER_ACCOUNT_NAME");
+  const ownerAccountEmail = getEnv("OWNER_ACCOUNT_EMAIL");
+  const githubClientId = getEnv("GITHUB_CLIENT_ID");
+  const githubClientSecret = getEnv("GITHUB_CLIENT_SECRET");
 
-  if (HOST === undefined) {
-    throw new Error("HOST is not defined");
-  } else if (PORT === undefined) {
-    throw new Error("PORT is not defined");
-  } else if (DATABASE_URL === undefined) {
-    throw new Error("DATABASE_URL is not defined");
-  } else if (SESSION_SECRET === undefined) {
-    throw new Error("SESSION_SECRET is not defined");
-  } else if (OWNER_ACCOUNT_PROVIDER === undefined) {
-    throw new Error("OWNER_ACCOUNT_PROVIDER is not defined");
-  } else if (OWNER_ACCOUNT_ID === undefined) {
-    throw new Error("OWNER_ACCOUNT_ID is not defined");
-  } else if (OWNER_ACCOUNT_NAME === undefined) {
-    throw new Error("OWNER_ACCOUNT_NAME is not defined");
-  } else if (OWNER_ACCOUNT_EMAIL === undefined) {
-    throw new Error("OWNER_ACCOUNT_EMAIL is not defined");
-  } else if (GITHUB_CLIENT_ID === undefined) {
-    throw new Error("GITHUB_CLIENT_ID is not defined");
-  } else if (GITHUB_CLIENT_SECRET === undefined) {
-    throw new Error("GITHUB_CLIENT_SECRET is not defined");
+  if (isNaN(serverPort)) {
+    throw new Error("SERVER_PORT is invalid");
   }
-
-  if (!isAuthProviderName(OWNER_ACCOUNT_PROVIDER)) {
+  if (!isAuthProviderName(ownerAccountProvider)) {
     throw new Error("OWNER_ACCOUNT_PROVIDER is invalid");
   }
 
   return {
-    serverHost: HOST,
-    serverPort: Number(PORT),
-    databaseUrl: DATABASE_URL,
-    sessionSecret: SESSION_SECRET,
-    ownerAccountProvider: OWNER_ACCOUNT_PROVIDER,
-    ownerAccountId: OWNER_ACCOUNT_ID,
-    ownerAccountName: OWNER_ACCOUNT_NAME,
-    ownerAccountEmail: OWNER_ACCOUNT_EMAIL,
-    githubClientId: GITHUB_CLIENT_ID,
-    githubClientSecret: GITHUB_CLIENT_SECRET
+    serverHost,
+    serverPort,
+    databaseUrl,
+    sessionSecret,
+    ownerAccountProvider,
+    ownerAccountId,
+    ownerAccountName,
+    ownerAccountEmail,
+    githubClientId,
+    githubClientSecret
   };
+};
+
+const getEnv = (key: string) => {
+  const value = process.env[key];
+  if (value === undefined) {
+    throw new Error(`${key} is not defined`);
+  }
+
+  return value;
 };
