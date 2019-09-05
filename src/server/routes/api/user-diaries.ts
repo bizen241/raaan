9603ccb5ffb53c22f300a9d64b5b83b2cb70b960
bizen241 <bizen241@gmail.com>
2019-09-05 12:20:13 +0900

    chore(api): add GET /api/user-diaries

---
+++ src/server/routes/api/user-diaries.ts
@@ -0,0 +1,39 @@
+import { OperationFunction } from "express-openapi";
+import * as createError from "http-errors";
+import { getManager } from "typeorm";
+import { UserDiary } from "../../../shared/api/entities";
+import { createOperationDoc, errorBoundary } from "../../api/operation";
+import { parseSearchParams } from "../../api/request/search/parse";
+import { responseSearchResult } from "../../api/response";
+import { UserDiaryEntity } from "../../database/entities";
+
+export const GET: OperationFunction = errorBoundary(async (req, res, next, currentUser) => {
+  const { userId, limit, offset } = parseSearchParams<UserDiary>("UserDiary", req.query);
+
+  const isOwnDiaries = userId === currentUser.id;
+  if (!isOwnDiaries) {
+    return next(createError(403));
+  }
+
+  const query = await getManager()
+    .createQueryBuilder(UserDiaryEntity, "userDiary")
+    .leftJoinAndSelect("userDiary.user", "user")
+    .orderBy("userDiary.date", "DESC")
+    .take(limit)
+    .skip(offset);
+
+  if (userId !== undefined) {
+    query.andWhere("user.id = :userId", { userId });
+  }
+
+  const [userDiaries, count] = await query.getManyAndCount();
+
+  responseSearchResult(req, res, userDiaries, count);
+});
+
+GET.apiDoc = createOperationDoc({
+  entityType: "UserDiary",
+  summary: "Search user diaries",
+  permission: "Read",
+  hasQuery: true
+});
