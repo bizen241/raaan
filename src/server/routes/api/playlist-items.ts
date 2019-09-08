2ac5159e8dae5ddc3bbfa9a1fbc3b432fdf562e6
bizen241 <bizen241@gmail.com>
2019-09-08 15:42:54 +0900

    chore(api): add GET /api/playlist-items

--- src/server/routes/api/playlist-items.ts
+++ src/server/routes/api/playlist-items.ts
@@ -4,8 +4,52 @@ import { getManager } from "typeorm";
 import { PlaylistItem } from "../../../shared/api/entities";
 import { SaveParams } from "../../../shared/api/request/save";
 import { createOperationDoc, errorBoundary } from "../../api/operation";
-import { responseFindResult } from "../../api/response";
+import { parseSearchParams } from "../../api/request/search/parse";
+import { responseFindResult, responseSearchResult } from "../../api/response";
 import { ExerciseEntity, PlaylistEntity, PlaylistItemEntity } from "../../database/entities";
+
+export const GET: OperationFunction = errorBoundary(async (req, res, next, currentUser) => {
+  const { playlistId, exerciseId, limit, offset } = parseSearchParams<PlaylistItem>("PlaylistItem", req.query);
+
+  const manager = getManager();
+
+  const query = await manager
+    .createQueryBuilder(PlaylistItemEntity, "playlistItem")
+    .leftJoinAndSelect("playlistItem.playlist", "playlist")
+    .leftJoinAndSelect("playlistItem.exercise", "exercise")
+    .take(limit)
+    .skip(offset);
+
+  if (playlistId !== undefined) {
+    const playlist = await manager.findOne(PlaylistEntity, playlistId);
+    if (playlist === undefined) {
+      return next(createError(400));
+    }
+
+    const isAuthor = playlist.authorId !== currentUser.id;
+    if (playlist.isPrivate && !isAuthor) {
+      return next(createError(403));
+    }
+
+    query.andWhere("playlist.id = :playlistId", { playlistId });
+  } else {
+    query.andWhere("playlist.isPrivate = false");
+  }
+  if (exerciseId !== undefined) {
+    query.andWhere("exercise.id = :exerciseId", { exerciseId });
+  }
+
+  const [exerciseSummaries, count] = await query.getManyAndCount();
+
+  responseSearchResult(req, res, exerciseSummaries, count);
+});
+
+GET.apiDoc = createOperationDoc({
+  entityType: "PlaylistItem",
+  summary: "Search exercises",
+  permission: "Guest",
+  hasQuery: true
+});
 
 export const POST: OperationFunction = errorBoundary(async (req, res, next, currentUser) => {
   const { playlistId, exerciseId, memo = "" }: SaveParams<PlaylistItem> = req.body;
