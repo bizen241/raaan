4be62dc4da75de249e93691ff05a77195722a6ee
bizen241 <bizen241@gmail.com>
2019-09-08 13:26:02 +0900

    chore(api): add DELETE /api/playlists/{id}

--- src/server/routes/api/playlists/{id}.ts
+++ src/server/routes/api/playlists/{id}.ts
@@ -15,7 +15,8 @@ export const GET: OperationFunction = errorBoundary(async (req, res, next, curre
     return next(createError(404));
   }
 
-  if (playlist.isPrivate && playlist.authorId !== currentUser.id) {
+  const isAuthor = playlist.authorId === currentUser.id;
+  if (playlist.isPrivate && !isAuthor) {
     return next(createError(403));
   }
 
@@ -26,5 +27,32 @@ GET.apiDoc = createOperationDoc({
   entityType: "Playlist",
   summary: "Get a playlist",
   permission: "Guest",
+  hasId: true
+});
+
+export const DELETE: OperationFunction = errorBoundary(async (req, res, next, currentUser) => {
+  const { id: playlistId }: PathParams = req.params;
+
+  const manager = getManager();
+
+  const playlist = await manager.findOne(PlaylistEntity, playlistId);
+  if (playlist === undefined) {
+    return next(createError(404));
+  }
+
+  const isAuthor = playlist.authorId === currentUser.id;
+  if (!isAuthor) {
+    return next(createError(403));
+  }
+
+  await manager.remove(playlist);
+
+  responseFindResult(req, res);
+});
+
+DELETE.apiDoc = createOperationDoc({
+  entityType: "Playlist",
+  summary: "Delete a playlist",
+  permission: "Read",
   hasId: true
 });
