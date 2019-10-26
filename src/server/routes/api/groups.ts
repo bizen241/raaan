04768842bcacd95021663f6c0f9e7a4ba4425c36
bizen241 <bizen241@gmail.com>
2019-10-27 08:09:05 +0900

    chore(api): add POST /api/groups

---
+++ src/server/routes/api/groups.ts
@@ -0,0 +1,30 @@
+import { OperationFunction } from "express-openapi";
+import { getManager } from "typeorm";
+import { Group } from "../../../shared/api/entities";
+import { Params } from "../../../shared/api/request/params";
+import { createOperationDoc, errorBoundary } from "../../api/operation";
+import { responseFindResult } from "../../api/response";
+import { GroupEntity, GroupMemberEntity } from "../../database/entities";
+
+export const POST: OperationFunction = errorBoundary(async (req, res, _, currentUser) => {
+  const { name = "", description = "" }: Params<Group> = req.body;
+
+  await getManager().transaction(async manager => {
+    const group = new GroupEntity(currentUser, name);
+    group.description = description;
+    await manager.save(group);
+
+    const groupOwner = new GroupMemberEntity(group, currentUser);
+    groupOwner.permission = "owner";
+    await manager.save(groupOwner);
+
+    responseFindResult(req, res, group, groupOwner);
+  });
+});
+
+POST.apiDoc = createOperationDoc({
+  entityType: "Group",
+  summary: "Create a group",
+  permission: "Write",
+  hasBody: true
+});
