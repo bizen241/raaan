c36ad754c8d9e502075218c707e6ddba14250774
bizen241 <bizen241@gmail.com>
2019-10-30 01:56:00 +0900

    chore(ui): add a dialog

---
+++ src/client/components/dialogs/user-sessions/DeleteUserSessionDialog.tsx
@@ -0,0 +1,28 @@
+import { Typography } from "@material-ui/core";
+import { Delete, Warning } from "@material-ui/icons";
+import * as React from "react";
+import { useDispatch } from "react-redux";
+import { createDialog, dialogTimeout } from "../../../enhancers/createDialog";
+import { actions } from "../../../reducers";
+import { Button, Card, DialogContent } from "../../ui";
+
+export const DeleteUserSessionDialog = createDialog<{
+  userSessionId: string;
+}>(
+  React.memo(({ userSessionId, onClose }) => {
+    const dispatch = useDispatch();
+
+    const onDelete = () => {
+      dispatch(actions.api.delete("UserSession", userSessionId, dialogTimeout, onClose));
+    };
+
+    return (
+      <DialogContent title="セッションの削除" onClose={onClose}>
+        <Card icon={<Warning />} title="セッションの削除">
+          <Typography>セッションがサーバーから削除されます。</Typography>
+        </Card>
+        <Button icon={<Delete color="error" />} label="セッションを削除" labelColor="error" onClick={onDelete} />
+      </DialogContent>
+    );
+  })
+);
--- src/client/components/list/UserSessionList.tsx
+++ src/client/components/list/UserSessionList.tsx
@@ -1,18 +1,19 @@
 import { Box, IconButton, TableCell, TableRow, Typography } from "@material-ui/core";
 import { Computer, Delete, Smartphone } from "@material-ui/icons";
 import * as React from "react";
-import { useCallback, useContext } from "react";
-import { useDispatch } from "react-redux";
+import { useContext } from "react";
 import { UserSession } from "../../../shared/api/entities";
 import { createEntityList } from "../../enhancers/createEntityList";
-import { actions } from "../../reducers";
+import { useToggleState } from "../../hooks/useToggleState";
+import { DeleteUserSessionDialog } from "../dialogs/user-sessions/DeleteUserSessionDialog";
 import { UserContext } from "../project/Context";
 import { Column } from "../ui";
 
 export const UserSessionList = createEntityList<UserSession>({ entityType: "UserSession", itemHeight: 77 })(
   React.memo(({ entity: userSession }) => {
-    const dispatch = useDispatch();
     const currentUser = useContext(UserContext);
+
+    const [isDeleteDialogOpen, onToggleDeleteDialog] = useToggleState();
 
     return (
       <TableRow>
@@ -28,11 +29,16 @@ export const UserSessionList = createEntityList<UserSession>({ entityType: "User
         </TableCell>
         <TableCell padding="checkbox">
           {userSession.userId === currentUser.id && !userSession.isCurrent ? (
-            <IconButton onClick={useCallback(() => dispatch(actions.api.delete("UserSession", userSession.id)), [])}>
+            <IconButton onClick={onToggleDeleteDialog}>
               <Delete />
             </IconButton>
           ) : null}
         </TableCell>
+        <DeleteUserSessionDialog
+          userSessionId={userSession.id}
+          isOpen={isDeleteDialogOpen}
+          onClose={onToggleDeleteDialog}
+        />
       </TableRow>
     );
   })
