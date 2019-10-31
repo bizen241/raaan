205debad2d848a81da60d081b780e3575d0a7134
bizen241 <bizen241@gmail.com>
2019-10-31 11:20:28 +0900

    chore(api): add GET /api/groups

--- src/server/routes/api/groups.ts
+++ src/server/routes/api/groups.ts
@@ -3,8 +3,29 @@ import { getManager } from "typeorm";
 import { Group } from "../../../shared/api/entities";
 import { Params } from "../../../shared/api/request/params";
 import { createOperationDoc, errorBoundary } from "../../api/operation";
-import { responseFindResult } from "../../api/response";
+import { parseQuery } from "../../api/request/search/parse";
+import { responseFindResult, responseSearchResult } from "../../api/response";
 import { GroupEntity, GroupMemberEntity } from "../../database/entities";
+
+export const GET: OperationFunction = errorBoundary(async (req, res) => {
+  const { searchLimit, searchOffset } = parseQuery<Group>("Group", req.query);
+
+  const query = await getManager()
+    .createQueryBuilder(GroupEntity, "group")
+    .leftJoinAndSelect("group.owner", "owner")
+    .take(searchLimit)
+    .skip(searchOffset);
+
+  const [groups, count] = await query.getManyAndCount();
+
+  responseSearchResult(req, res, groups, count);
+});
+
+GET.apiDoc = createOperationDoc({
+  entityType: "Group",
+  permission: "Guest",
+  hasQuery: true
+});
 
 export const POST: OperationFunction = errorBoundary(async (req, res, _, currentUser) => {
   const { name = "", description = "" }: Params<Group> = req.body;
