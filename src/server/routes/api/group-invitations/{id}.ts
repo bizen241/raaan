269129eb7096a17f8d7ff65ca1039184a3547f17
bizen241 <bizen241@gmail.com>
2019-11-11 10:05:09 +0900

    chore(api): add DELETE /api/group-invitations/{id}

---
+++ src/server/routes/api/group-invitations/{id}.ts
@@ -0,0 +1,37 @@
+import { OperationFunction } from "express-openapi";
+import * as createError from "http-errors";
+import { getManager } from "typeorm";
+import { createOperationDoc, errorBoundary, PathParams } from "../../../api/operation";
+import { responseFindResult } from "../../../api/response";
+import { GroupInvitationEntity } from "../../../database/entities";
+
+export const DELETE: OperationFunction = errorBoundary(async (req, res, next, currentUser) => {
+  const { id: groupInvitationId }: PathParams = req.params;
+
+  await getManager().transaction(async manager => {
+    const groupInvitation = await manager.findOne(GroupInvitationEntity, groupInvitationId, {
+      relations: ["group"]
+    });
+    if (groupInvitation === undefined) {
+      return next(createError(404));
+    }
+    if (groupInvitation.group === undefined) {
+      return next(createError(500));
+    }
+
+    const isOwner = groupInvitation.group.ownerId === currentUser.id;
+    if (!isOwner) {
+      return next(createError(403));
+    }
+
+    await manager.remove(groupInvitation);
+
+    responseFindResult(req, res);
+  });
+});
+
+DELETE.apiDoc = createOperationDoc({
+  entityType: "GroupInvitation",
+  permission: "Read",
+  hasId: true
+});
