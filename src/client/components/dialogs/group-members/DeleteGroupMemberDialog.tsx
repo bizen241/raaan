7455c4be4bdabe03cf2260001cc413f2501fea60
bizen241 <bizen241@gmail.com>
2019-11-12 14:13:15 +0900

    chore(group): delete a member

---
+++ src/client/components/dialogs/group-members/DeleteGroupMemberDialog.tsx
@@ -0,0 +1,28 @@
+import { Typography } from "@material-ui/core";
+import { RemoveCircle } from "@material-ui/icons";
+import * as React from "react";
+import { useDispatch } from "react-redux";
+import { createDialog, dialogTimeout } from "../../../enhancers/createDialog";
+import { actions } from "../../../reducers";
+import { Button, Card, DialogContent } from "../../ui";
+
+export const DeleteGroupMemberDialog = createDialog<{
+  groupMemberId: string;
+}>(
+  React.memo(({ groupMemberId, onClose }) => {
+    const dispatch = useDispatch();
+
+    const onDelete = () => {
+      dispatch(actions.api.delete("GroupMember", groupMemberId, dialogTimeout, onClose));
+    };
+
+    return (
+      <DialogContent title="グループからの脱退" onClose={onClose}>
+        <Card icon={<RemoveCircle />} title="グループからの脱退">
+          <Typography>グループから脱退します。</Typography>
+        </Card>
+        <Button icon={<RemoveCircle />} label="グループから脱退" onClick={onDelete} />
+      </DialogContent>
+    );
+  })
+);
--- src/client/components/viewer/GroupViewer.tsx
+++ src/client/components/viewer/GroupViewer.tsx
@@ -1,17 +1,49 @@
-import { Group as GroupIcon, Keyboard, Person } from "@material-ui/icons";
+import { Group as GroupIcon, Keyboard, Person, RemoveCircle } from "@material-ui/icons";
 import * as React from "react";
-import { Group } from "../../../shared/api/entities";
+import { useContext } from "react";
+import { Group, GroupMember } from "../../../shared/api/entities";
 import { withEntity } from "../../enhancers/withEntity";
-import { Button, Card, Column, Property } from "../ui";
+import { useSearch } from "../../hooks/useSearch";
+import { useToggleState } from "../../hooks/useToggleState";
+import { DeleteGroupMemberDialog } from "../dialogs/group-members/DeleteGroupMemberDialog";
+import { UserContext } from "../project/Context";
+import { Button, Card, Column, Menu, MenuItem, Property } from "../ui";
 
 export const GroupViewer = withEntity<Group, {}>({ entityType: "Group" })(({ entityId: groupId, entity: group }) => {
+  const currentUser = useContext(UserContext);
+
+  const [isDeleteDialogOpen, onToggleDeleteDialog] = useToggleState();
+
+  const { entities: groupMembers } = useSearch<GroupMember>("GroupMember", {
+    groupId,
+    userId: currentUser.id
+  });
+  const groupMember = groupMembers[0];
+
   return (
     <Column>
       <Button icon={<Keyboard />} label="クイズ" to={`/groups/${groupId}/group-exercises`} />
       <Button icon={<Person />} label="メンバー" to={`/groups/${groupId}/group-members`} />
-      <Card icon={<GroupIcon />} title={group.name}>
+      <Card
+        icon={<GroupIcon />}
+        title={group.name}
+        action={
+          <Menu>
+            {groupMember && groupMember.permission !== "owner" && (
+              <MenuItem icon={<RemoveCircle />} label="脱退" onClick={onToggleDeleteDialog} />
+            )}
+          </Menu>
+        }
+      >
         <Property label="説明">{group.description}</Property>
       </Card>
+      {groupMember && (
+        <DeleteGroupMemberDialog
+          groupMemberId={groupMember.id}
+          isOpen={isDeleteDialogOpen}
+          onClose={onToggleDeleteDialog}
+        />
+      )}
     </Column>
   );
 });
