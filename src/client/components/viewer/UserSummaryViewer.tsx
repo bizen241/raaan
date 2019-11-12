a4f9df8d49e3cc90f90ca68cae9b4dbe6bea64fe
bizen241 <bizen241@gmail.com>
2019-11-13 08:29:00 +0900

    chore(group): invite from UserPage

--- src/client/components/viewer/UserSummaryViewer.tsx
+++ src/client/components/viewer/UserSummaryViewer.tsx
@@ -1,36 +1,40 @@
-import { Avatar, Card, CardContent, CardHeader, Typography } from "@material-ui/core";
-import { Person } from "@material-ui/icons";
+import { Email, Person } from "@material-ui/icons";
 import * as React from "react";
 import { useContext } from "react";
 import { UserSummary } from "../../../shared/api/entities";
 import { withEntity } from "../../enhancers/withEntity";
+import { useToggleState } from "../../hooks/useToggleState";
+import { GroupInvitationsDialog } from "../dialogs/user-follows/GroupInvitationsDialog";
 import { UserContext } from "../project/Context";
-import { Column, Property } from "../ui";
-import { useStyles } from "../ui/styles";
+import { Card, Menu, MenuItem, Property } from "../ui";
 
 export const UserSummaryViewer = withEntity<UserSummary>({ entityType: "UserSummary" })(
   React.memo(({ entity: userSummary }) => {
-    const classes = useStyles();
     const currentUser = useContext(UserContext);
+
+    const [isGroupInvitationsDialogOpen, toggleGroupInvitationsDialog] = useToggleState();
 
     const isOwn = userSummary.userId === currentUser.id;
 
     return (
-      <Card>
-        <CardHeader
-          avatar={
-            <Avatar className={classes.cardAvatar}>
-              <Person />
-            </Avatar>
-          }
-          title={<Typography>{isOwn ? "自分の情報" : "ユーザーの情報"}</Typography>}
+      <Card
+        icon={<Person />}
+        title={isOwn ? "自分の情報" : "ユーザーの情報"}
+        action={
+          !isOwn && (
+            <Menu>
+              <MenuItem icon={<Email />} label="グループに招待" onClick={toggleGroupInvitationsDialog} />
+            </Menu>
+          )
+        }
+      >
+        <Property label="提出回数">{userSummary.submitCount}</Property>
+        <Property label="打鍵回数">{userSummary.typeCount}</Property>
+        <GroupInvitationsDialog
+          followerId={userSummary.userId}
+          isOpen={isGroupInvitationsDialogOpen}
+          onClose={toggleGroupInvitationsDialog}
         />
-        <CardContent>
-          <Column>
-            <Property label="提出回数">{userSummary.submitCount}</Property>
-            <Property label="打鍵回数">{userSummary.typeCount}</Property>
-          </Column>
-        </CardContent>
       </Card>
     );
   })
