89c945bf71678736d686cea38ab642b73c627949
bizen241 <bizen241@gmail.com>
2019-11-14 15:03:15 +0900

    chore(api): add PATCH /api/group-secrets/{id}

--- src/server/routes/api/group-secrets/{id}.ts
+++ src/server/routes/api/group-secrets/{id}.ts
@@ -1,6 +1,9 @@
 import { OperationFunction } from "express-openapi";
 import * as createError from "http-errors";
 import { getManager } from "typeorm";
+import * as uuid from "uuid/v4";
+import { GroupSecret } from "../../../../shared/api/entities";
+import { Params } from "../../../../shared/api/request/params";
 import { createOperationDoc, errorBoundary, PathParams } from "../../../api/operation";
 import { responseFindResult } from "../../../api/response";
 import { GroupSecretEntity } from "../../../database/entities";
@@ -28,4 +31,41 @@ GET.apiDoc = createOperationDoc({
   entityType: "GroupSecret",
   permission: "Read",
   hasId: true
+});
+
+export const PATCH: OperationFunction = errorBoundary(async (req, res, next, currentUser) => {
+  const { id: groupSecretId }: PathParams = req.params;
+  const { expireAt }: Params<GroupSecret> = req.body;
+  if (expireAt === undefined) {
+    return next(createError(400));
+  }
+
+  const manager = getManager();
+
+  const groupSecret = await manager.findOne(GroupSecretEntity, groupSecretId, {
+    relations: ["group"]
+  });
+  if (groupSecret === undefined) {
+    return next(createError(404));
+  }
+  if (groupSecret.group === undefined) {
+    return next(createError(500));
+  }
+  if (groupSecret.group.ownerId !== currentUser.id) {
+    return next(createError(403));
+  }
+
+  groupSecret.value = uuid();
+  groupSecret.expireAt = new Date(expireAt);
+
+  await manager.save(groupSecret);
+
+  responseFindResult(req, res, groupSecret);
+});
+
+PATCH.apiDoc = createOperationDoc({
+  entityType: "GroupSecret",
+  permission: "Admin",
+  hasId: true,
+  hasBody: true
 });
