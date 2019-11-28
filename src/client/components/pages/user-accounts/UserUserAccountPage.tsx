a93a054caf4b559afdd29705546a64875a9f0152
bizen241 <bizen241@gmail.com>
2019-11-28 10:00:02 +0900

    chore(client): add UserAccountViewer

--- src/client/components/pages/user-accounts/UserUserAccountPage.tsx
+++ src/client/components/pages/user-accounts/UserUserAccountPage.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../reducers";
 import { Button } from "../../ui";
 import { Page } from "../../ui/Page";
+import { UserAccountViewer } from "../../viewer/UserAccountViewer";
 
 export const UserUserAccountPage = () => {
+  const currentUserAccountId = useSelector((state: RootState) => state.app.userAccountId);
+
   return (
     <Page title="プロバイダの設定">
       <Button label="メールアドレスの更新" />
       <Button label="プロバイダの変更" />
+      <UserAccountViewer entityId={currentUserAccountId} />
     </Page>
   );
 };
---
+++ src/client/components/viewer/UserAccountViewer.tsx
@@ -0,0 +1,17 @@
+import { Lock } from "@material-ui/icons";
+import * as React from "react";
+import { UserAccount } from "../../../shared/api/entities";
+import { withEntity } from "../../enhancers/withEntity";
+import { Card, Property } from "../ui";
+
+export const UserAccountViewer = withEntity<UserAccount>({ entityType: "UserAccount" })(
+  React.memo(({ entity: userAccount }) => {
+    return (
+      <Card icon={<Lock />} title="現在のプロバイダ">
+        <Property label="プロバイダ名">{userAccount.provider}</Property>
+        <Property label="アカウントID">{userAccount.accountId}</Property>
+        <Property label="メールアドレス">{userAccount.email}</Property>
+      </Card>
+    );
+  })
+);
