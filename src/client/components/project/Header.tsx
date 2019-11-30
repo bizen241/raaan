13eecf0eac0b39fe6ae33323d8ee47c4dac4b47c
bizen241 <bizen241@gmail.com>
2019-11-30 11:58:47 +0900

    chore(ui): display avatar icon

--- src/client/components/project/Header.tsx
+++ src/client/components/project/Header.tsx
@@ -1,18 +1,21 @@
-import { AppBar, Box, IconButton, MenuItem, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Avatar, Box, IconButton, Toolbar, Typography } from "@material-ui/core";
 import { AccountCircle, ArrowBack, Home, Info, Mail, Settings } from "@material-ui/icons";
 import { goBack } from "connected-react-router";
 import * as React from "react";
-import { useCallback } from "react";
+import { useCallback, useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { UserSummary } from "../../../shared/api/entities";
+import { useEntity } from "../../hooks/useEntity";
 import { RootState } from "../../reducers";
-import { Column, Menu } from "../ui";
-import { useStyles } from "../ui/styles";
+import { Column, Menu, MenuItem } from "../ui";
+import { UserContext } from "./Context";
 import { Message } from "./Message";
 
 export const Header = React.memo<{ title?: React.ReactNode }>(({ title = "" }) => {
-  const classes = useStyles();
   const dispatch = useDispatch();
+  const currentUser = useContext(UserContext);
+
+  const { entity: currentUserSummary } = useEntity<UserSummary>("UserSummary", currentUser.summaryId);
 
   const { pathname } = useSelector((state: RootState) => ({
     pathname: state.router.location.pathname
@@ -28,31 +31,30 @@ export const Header = React.memo<{ title?: React.ReactNode }>(({ title = "" }) =
             </IconButton>
             <Typography component="span">{title}</Typography>
             <Box flex={1} />
+            {currentUserSummary && currentUserSummary.emailHash && (
+              <Avatar src={`https://www.gravatar.com/avatar/${currentUserSummary.emailHash}?d=mp`} />
+            )}
             <Menu>
-              <MenuItem disabled={pathname === "/user/user-config"} component={Link} to="/user/user-config">
-                <Settings className={classes.leftIcon} />
-                <Typography>
-                  <Message id="settings" />
-                </Typography>
-              </MenuItem>
-              <MenuItem disabled={pathname === "/"} component={Link} to="/">
-                <Home className={classes.leftIcon} />
-                <Typography>ホーム</Typography>
-              </MenuItem>
-              <MenuItem disabled={pathname === "/user/user-messages"} component={Link} to="/user/user-messages">
-                <Mail className={classes.leftIcon} />
-                <Typography>通知</Typography>
-              </MenuItem>
-              <MenuItem disabled={pathname === "/user"} component={Link} to="/user">
-                <AccountCircle className={classes.leftIcon} />
-                <Typography>
-                  <Message id="account" />
-                </Typography>
-              </MenuItem>
-              <MenuItem disabled={pathname === "/app"} component={Link} to="/app">
-                <Info className={classes.leftIcon} />
-                アプリについて
-              </MenuItem>
+              <MenuItem
+                icon={<Settings />}
+                label={<Message id="settings" />}
+                disabled={pathname === "/user/user-config"}
+                to="/user/user-config"
+              />
+              <MenuItem icon={<Home />} label="ホーム" disabled={pathname === "/"} to="/" />
+              <MenuItem
+                icon={<Mail />}
+                label="通知"
+                disabled={pathname === "/user/user-messages"}
+                to="/user/user-messages"
+              />
+              <MenuItem
+                icon={<AccountCircle />}
+                label={<Message id="account" />}
+                disabled={pathname === "/user"}
+                to="/user"
+              />
+              <MenuItem icon={<Info />} label="アプリについて" disabled={pathname === "/app"} to="/app" />
             </Menu>
           </Toolbar>
         </Column>
