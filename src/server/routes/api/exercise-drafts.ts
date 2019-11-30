9bedcc71193b39cfbf67c8647772f71373b9f418
bizen241 <bizen241@gmail.com>
2019-12-01 06:26:50 +0900

    fix(api): normalize exercise

--- src/server/routes/api/exercise-drafts.ts
+++ src/server/routes/api/exercise-drafts.ts
@@ -1,19 +1,22 @@
 import { OperationFunction } from "express-openapi";
+import * as createError from "http-errors";
 import { getManager } from "typeorm";
 import { ExerciseDraft } from "../../../shared/api/entities";
 import { Params } from "../../../shared/api/request/params";
 import { getMinMaxTypeCount } from "../../../shared/exercise";
 import { createOperationDoc, errorBoundary } from "../../api/operation";
 import { responseFindResult } from "../../api/response";
-import { ExerciseDraftEntity, ExerciseEntity, ExerciseSummaryEntity } from "../../database/entities";
+import { ExerciseDraftEntity, ExerciseEntity, ExerciseSummaryEntity, UserSummaryEntity } from "../../database/entities";
 import { getTags } from "../../services/tags";
 
 export const POST: OperationFunction = errorBoundary(async (req, res, _, currentUser) => {
   const { isMerged = true, isPrivate = true, ...params }: Params<ExerciseDraft> = req.body;
 
   await getManager().transaction(async manager => {
-    const exerciseDraft = new ExerciseDraftEntity(params);
-    exerciseDraft.isMerged = isMerged;
+    const authorSummary = await manager.findOne(UserSummaryEntity, currentUser.summaryId);
+    if (authorSummary === undefined) {
+      throw createError(500, "authorSummary is not defined");
+    }
 
     const exerciseSummary = new ExerciseSummaryEntity();
     if (isMerged) {
@@ -24,6 +27,9 @@ export const POST: OperationFunction = errorBoundary(async (req, res, _, current
       exerciseSummary.tags = await getTags(exerciseSummary, params, manager);
     }
 
+    const exerciseDraft = new ExerciseDraftEntity(params);
+    exerciseDraft.isMerged = isMerged;
+
     const exercise = new ExerciseEntity(isMerged ? params : {});
     exercise.author = currentUser;
     exercise.summary = exerciseSummary;
@@ -32,9 +38,11 @@ export const POST: OperationFunction = errorBoundary(async (req, res, _, current
     exercise.isPrivate = isPrivate;
     await manager.save(exercise);
 
-    exerciseSummary.exercise = exercise;
+    exercise.author.summary = authorSummary;
+    exercise.author.summary.user = exercise.author;
+    exercise.summary.exercise = exercise;
 
-    responseFindResult(req, res, exercise, exerciseSummary);
+    responseFindResult(req, res, exercise);
   });
 });
 
