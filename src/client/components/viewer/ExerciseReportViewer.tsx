53b232fefbdc968d6d71047b5e4f935f13009509
bizen241 <bizen241@gmail.com>
2019-12-03 10:45:30 +0900

    chore(viewer): add a link button

--- src/client/components/viewer/ExerciseReportViewer.tsx
+++ src/client/components/viewer/ExerciseReportViewer.tsx
@@ -1,13 +1,21 @@
-import { Report } from "@material-ui/icons";
+import { Gavel, Report } from "@material-ui/icons";
 import * as React from "react";
 import { ExerciseReport } from "../../../shared/api/entities";
 import { withEntity } from "../../enhancers/withEntity";
-import { Card, Column, Property } from "../ui";
+import { Button, Card, Column, Property } from "../ui";
 
 export const ExerciseReportViewer = withEntity<ExerciseReport, {}>({ entityType: "ExerciseReport" })(
   ({ entity: exerciseReport }) => {
+    const { state } = exerciseReport;
+
     return (
       <Column>
+        <Button
+          color="primary"
+          icon={<Gavel />}
+          label={state === "pending" ? "対応する" : "編集する"}
+          to={`/exercise-reports/${exerciseReport.id}/edit`}
+        />
         <Card icon={<Report />} title="クイズの報告">
           <Property label="理由">{exerciseReport.reason}</Property>
           <Property label="状態">{exerciseReport.state}</Property>
