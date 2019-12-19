6b01539d96b1115120a8e9c6eeb33b54a04a4fc7
bizen241 <bizen241@gmail.com>
2019-12-19 14:26:45 +0900

    chore(entity): delete unused classes

--- src/shared/api/entities/BaseObjectionObejct.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntityObject, UUID } from "./BaseEntityObject";
-import { ObjectionState } from "./Objection";
-
-export interface BaseObjectionObject extends BaseEntityObject {
-  objectorId?: UUID;
-  targetId: UUID;
-  comment: string;
-  state: ObjectionState;
-}
