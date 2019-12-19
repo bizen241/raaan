d3e9cc95d698f051f015817372444c3d0160752a
bizen241 <bizen241@gmail.com>
2019-12-19 21:41:33 +0900

    chore(api): add GET /api/suggestion-summaries

---
+++ src/server/routes/api/suggestion-summaries.ts
@@ -0,0 +1,34 @@
+import { OperationFunction } from "express-openapi";
+import { getManager } from "typeorm";
+import { parseQuery } from "../../../shared/api/request/parse";
+import { createOperationDoc, errorBoundary } from "../../api/operation";
+import { responseSearchResult } from "../../api/response";
+import { SuggestionSummaryEntity } from "../../database/entities";
+
+export const GET: OperationFunction = errorBoundary(async (req, res) => {
+  const { authorId, exerciseId, searchLimit, searchOffset } = parseQuery("SuggestionSummary", req.query);
+
+  const query = getManager()
+    .createQueryBuilder(SuggestionSummaryEntity, "suggestionSummary")
+    .leftJoinAndSelect("suggestionSummary.suggestion", "suggestion")
+    .leftJoinAndSelect("suggestion.revision", "revision")
+    .take(searchLimit)
+    .skip(searchOffset);
+
+  if (authorId !== undefined) {
+    query.andWhere("suggestion.authorId = :authorId", { authorId });
+  }
+  if (exerciseId !== undefined) {
+    query.andWhere("revision.exerciseId = :exerciseId", { exerciseId });
+  }
+
+  const [suggestionSummaries, count] = await query.getManyAndCount();
+
+  responseSearchResult(req, res, suggestionSummaries, count);
+});
+
+GET.apiDoc = createOperationDoc({
+  entityType: "SuggestionSummary",
+  permission: "Read",
+  hasQuery: true
+});
