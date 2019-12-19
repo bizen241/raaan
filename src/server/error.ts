5d6e5ffdd46830a4e86f471a663a5751ffa2124a
bizen241 <bizen241@gmail.com>
2019-12-19 14:33:35 +0900

    fix(server): fix error handler

--- src/server/error.ts
+++ src/server/error.ts
@@ -1,7 +1,7 @@
 import * as express from "express";
 import { HttpError } from "http-errors";
 
-export const errorHandler: express.ErrorRequestHandler = (error: HttpError, _, res) => {
+export const errorHandler: express.ErrorRequestHandler = (error: HttpError, _, res, __) => {
   res.status(error.status || 500);
   res.json({
     status,
