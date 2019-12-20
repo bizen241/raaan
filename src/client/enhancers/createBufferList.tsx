c6a72d25a9f2ff746ac246f27ea1e06d1bd72f23
bizen241 <bizen241@gmail.com>
2019-12-20 14:51:28 +0900

    chore(ui): update pagination

--- src/client/enhancers/createBufferList.tsx
+++ src/client/enhancers/createBufferList.tsx
@@ -1,14 +1,14 @@
-import { Card, Table, TableBody, TableCell, TableFooter, TablePagination, TableRow } from "@material-ui/core";
+import { Card, Table, TableBody, TableCell, TableRow } from "@material-ui/core";
 import * as React from "react";
 import { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import { EntityObject, EntityType, EntityTypeToEntity } from "../../shared/api/entities";
 import { Params } from "../../shared/api/request/params";
+import { Column, TablePagination } from "../components/ui";
 import { RootState } from "../reducers";
 
 interface BufferListProps {
   title?: React.ReactNode;
-  elevation?: number;
 }
 
 interface BufferListItemProps<E extends EntityObject> {
@@ -20,45 +20,43 @@ interface BufferListItemProps<E extends EntityObject> {
 export const createBufferList = <T extends EntityType>(entityType: T) => (
   ListItem: React.ComponentType<BufferListItemProps<EntityTypeToEntity[T]>>
 ) =>
-  React.memo<BufferListProps>(({ elevation }) => {
+  React.memo<BufferListProps>(() => {
     const { bufferMap, entityMap } = useSelector((state: RootState) => ({
       bufferMap: state.buffers[entityType],
       entityMap: state.cache.get[entityType] as { [key: string]: EntityTypeToEntity[T] | undefined }
     }));
 
     const [limit, setLimit] = useState(10);
-    const [page, setPage] = useState(0);
+    const [offset, setOffset] = useState(0);
 
     const bufferEntries = Object.entries(bufferMap);
-    const offset = limit * page;
     const emptyRows = limit - bufferEntries.length;
 
     return (
-      <Card elevation={elevation}>
-        <Table>
-          <TableBody>
-            {bufferEntries.slice(offset, offset + limit).map(([bufferId, buffer]) => (
-              <ListItem key={bufferId} bufferId={bufferId} buffer={buffer} source={entityMap[bufferId]} />
-            ))}
-            {emptyRows && (
-              <TableRow style={{ height: 53 * emptyRows }}>
-                <TableCell colSpan={3} />
-              </TableRow>
-            )}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPage={limit}
-                page={page}
-                count={bufferEntries.length}
-                labelRowsPerPage="表示件数:"
-                onChangePage={useCallback((_, newPage) => setPage(newPage), [])}
-                onChangeRowsPerPage={useCallback(e => setLimit(parseInt(e.target.value, 10)), [])}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </Card>
+      <Column pb={1}>
+        <Card>
+          <Table>
+            <TableBody>
+              {bufferEntries.slice(offset, offset + limit).map(([bufferId, buffer]) => (
+                <ListItem key={bufferId} bufferId={bufferId} buffer={buffer} source={entityMap[bufferId]} />
+              ))}
+              {emptyRows && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={3} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+          <Column p={2}>
+            <TablePagination
+              rowsPerPage={limit}
+              page={offset / limit}
+              count={bufferEntries.length}
+              onChangePage={useCallback((page: number) => setOffset(page * limit), [limit])}
+              onChangeRowsPerPage={useCallback((rowsPerPage: number) => setLimit(rowsPerPage), [])}
+            />
+          </Column>
+        </Card>
+      </Column>
     );
   });
