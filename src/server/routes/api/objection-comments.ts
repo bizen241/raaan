01be854159dac554a7b8936d5b3ef9ccb37dd843
bizen241 <bizen241@gmail.com>
2019-12-30 01:57:06 +0900

    feat(api): add POST /api/objection-comments

--- src/server/routes/api/objection-comments.ts
+++ src/server/routes/api/objection-comments.ts
@@ -1,9 +1,12 @@
 import { OperationFunction } from "express-openapi";
+import * as createError from "http-errors";
 import { getManager } from "typeorm";
+import { ObjectionComment } from "../../../shared/api/entities";
+import { Params } from "../../../shared/api/request/params";
 import { parseQuery } from "../../../shared/api/request/parse";
 import { createOperationDoc, errorBoundary } from "../../api/operation";
-import { responseSearchResult } from "../../api/response";
-import { ObjectionCommentEntity } from "../../database/entities";
+import { responseFindResult, responseSearchResult } from "../../api/response";
+import { ObjectionCommentEntity, ObjectionEntity } from "../../database/entities";
 
 export const GET: OperationFunction = errorBoundary(async (req, res) => {
   const { targetId, authorId, searchLimit, searchOffset } = parseQuery("ObjectionComment", req.query);
@@ -32,4 +35,26 @@ GET.apiDoc = createOperationDoc({
   entityType: "ObjectionComment",
   permission: "Read",
   hasQuery: true
+});
+
+export const POST: OperationFunction = errorBoundary(async (req, res, _, currentUser) => {
+  const { targetId, body = "" }: Params<ObjectionComment> = req.body;
+
+  await getManager().transaction(async manager => {
+    const target = await manager.findOne(ObjectionEntity, targetId);
+    if (target === undefined) {
+      throw createError(404);
+    }
+
+    const objectionComment = new ObjectionCommentEntity(target, currentUser, body);
+    await manager.save(objectionComment);
+
+    responseFindResult(req, res, objectionComment);
+  });
+});
+
+POST.apiDoc = createOperationDoc({
+  entityType: "ObjectionComment",
+  permission: "Write",
+  hasBody: true
 });
