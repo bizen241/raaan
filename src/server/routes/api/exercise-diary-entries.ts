8d0790a8b1ebcd12d8bc76763175df5edd2e32d4
bizen241 <bizen241@gmail.com>
2020-01-08 00:37:40 +0900

    feat(api): add GET /api/exercise-diary-entries

---
+++ src/server/routes/api/exercise-diary-entries.ts
@@ -0,0 +1,17 @@
+import { createSearchOperation } from "../../api/operation";
+import { ExerciseDiaryEntryEntity } from "../../database/entities";
+
+export const GET = createSearchOperation("ExerciseDiaryEntry", "Read", async ({ manager, params }) => {
+  const { exerciseId } = params;
+
+  const query = manager
+    .createQueryBuilder(ExerciseDiaryEntryEntity, "exerciseDiaryEntry")
+    .leftJoinAndSelect("exerciseDiaryEntry.exercise", "exercise")
+    .orderBy("exerciseDiaryEntry.date", "DESC");
+
+  if (exerciseId !== undefined) {
+    query.andWhere("exercise.id = :exerciseId", { exerciseId });
+  }
+
+  return query;
+});
