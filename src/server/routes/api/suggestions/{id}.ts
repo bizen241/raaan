369327c9f20afb4dba5a187bafcd3885b0dd0f2e
bizen241 <bizen241@gmail.com>
2020-01-11 00:17:34 +0900

    chore(api): update state of suggestion

--- src/server/routes/api/suggestions/{id}.ts
+++ src/server/routes/api/suggestions/{id}.ts
@@ -15,25 +15,37 @@ export const GET = createGetOperation("Suggestion", "Read", async ({ manager, id
 
 export const PATCH = createPatchOperation("Suggestion", "Write", async ({ currentUser, manager, id, params }) => {
   const suggestion = await manager.findOne(SuggestionEntity, id, {
-    relations: ["summary", "revision"]
+    relations: ["summary", "revision", "revision.exercise"]
   });
   if (suggestion === undefined) {
     throw createError(404);
   }
+  if (suggestion.revision === undefined || suggestion.revision.exercise === undefined) {
+    throw createError(500);
+  }
 
-  const isAuthor = suggestion.authorId === currentUser.id;
-  if (!isAuthor) {
+  const isSuggestionAuthor = suggestion.authorId === currentUser.id;
+  const isExerciseAuthor = suggestion.revision.exercise.authorId === currentUser.id;
+  if (!isSuggestionAuthor && !isExerciseAuthor) {
     throw createError(403);
   }
 
-  if (params.title !== undefined) {
-    suggestion.title = params.title;
-  }
-  if (params.tags !== undefined) {
-    suggestion.tags = params.tags;
-  }
-  if (params.questions !== undefined) {
-    suggestion.questions = params.questions;
+  if (isSuggestionAuthor) {
+    if (params.title !== undefined) {
+      suggestion.title = params.title;
+    }
+    if (params.tags !== undefined) {
+      suggestion.tags = params.tags;
+    }
+    if (params.questions !== undefined) {
+      suggestion.questions = params.questions;
+    }
+  } else {
+    const state = params.state;
+
+    if (state !== undefined && state !== "accepted") {
+      suggestion.state = state;
+    }
   }
 
   await manager.save(suggestion);
