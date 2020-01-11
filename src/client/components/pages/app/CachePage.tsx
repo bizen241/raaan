e5ade6fa43f742bc21a552f8ab6bcead89b811c8
bizen241 <bizen241@gmail.com>
2020-01-11 23:12:51 +0900

    fix(page): fix delete buttons

--- src/client/components/pages/app/CachePage.tsx
+++ src/client/components/pages/app/CachePage.tsx
@@ -1,16 +1,15 @@
 import { IconButton, Table, TableBody, TableCell, TableRow, Typography } from "@material-ui/core";
 import { Delete } from "@material-ui/icons";
 import * as React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { createEntityTypeToObject, EntityType } from "../../../../shared/api/entities";
-import { actions, RootState } from "../../../reducers";
+import { actions } from "../../../reducers";
 import { Button, Card, Page } from "../../ui";
 
 const entityTypes = Object.keys(createEntityTypeToObject()) as EntityType[];
 
 export const CachePage = React.memo(() => {
   const dispatch = useDispatch();
-  const cache = useSelector((state: RootState) => state.cache);
 
   return (
     <Page title="キャッシュ">
@@ -25,16 +24,13 @@ export const CachePage = React.memo(() => {
         <Table>
           <TableBody>
             {entityTypes.map(entityType => (
-              <TableRow>
+              <TableRow key={entityType}>
                 <TableCell>
                   <Typography>{entityType}</Typography>
                 </TableCell>
-                <TableCell align="right">
-                  <Typography>{Object.values(cache.get[entityType]).length}</Typography>
-                </TableCell>
                 <TableCell padding="checkbox">
-                  <IconButton>
-                    <Delete onClick={() => dispatch(actions.cache.purge(entityType, undefined))} />
+                  <IconButton onClick={() => dispatch(actions.cache.purge(entityType, undefined))}>
+                    <Delete />
                   </IconButton>
                 </TableCell>
               </TableRow>
