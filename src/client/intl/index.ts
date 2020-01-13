d7ee6f2126b1854c8dd0acf2dc21602341825c1b
bizen241 <bizen241@gmail.com>
2020-01-13 13:48:34 +0900

    feat(intl): move translations in JSON files

--- src/client/intl/index.ts
+++ src/client/intl/index.ts
@@ -2,24 +2,8 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: {
-        "pages.HomePage.title": "Home",
-        "editor.UserConfigEditor.label.language": "language",
-        "editor.UserConfigEditor.label.theme": "theme"
-      }
-    },
-    ja: {
-      translation: {
-        "pages.HomePage.title": "ホーム",
-        "editor.UserConfigEditor.label.language": "言語",
-        "editor.UserConfigEditor.label.theme": "テーマ"
-      }
-    }
-  },
   lng: "ja",
-  fallbackLng: "ja",
+  fallbackLng: false,
   keySeparator: false,
   interpolation: {
     escapeValue: false
