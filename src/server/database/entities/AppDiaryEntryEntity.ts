45c5ba315bbc6dd3ba3376d8107ac28daf02da42
bizen241 <bizen241@gmail.com>
2020-01-21 01:19:18 +0900

    refactor(entity): add BaseDiaryEntryClass

--- src/server/database/entities/AppDiaryEntryEntity.ts
+++ src/server/database/entities/AppDiaryEntryEntity.ts
@@ -1,22 +1,13 @@
 import { Column, Entity } from "typeorm";
-import { BaseEntityClass } from "./BaseEntityClass";
+import { BaseDiaryEntryClass } from "./BaseDiaryEntryClass";
 
 @Entity("app_diaries")
-export class AppDiaryEntryEntity extends BaseEntityClass {
+export class AppDiaryEntryEntity extends BaseDiaryEntryClass {
   type: "AppDiaryEntry" = "AppDiaryEntry";
-
-  @Column("date")
-  date: string;
 
   @Column()
   submittedCount: number = 0;
 
   @Column()
   typedCount: number = 0;
-
-  constructor(date: string) {
-    super();
-
-    this.date = date;
-  }
 }
