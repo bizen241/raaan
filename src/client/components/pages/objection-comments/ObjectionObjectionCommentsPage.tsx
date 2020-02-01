ec6c5781d3f6594b8870f1319076bc5c2df6661e
bizen241 <bizen241@gmail.com>
2020-02-01 12:20:32 +0900

    chore(ui): use Loading

--- src/client/components/pages/objection-comments/ObjectionObjectionCommentsPage.tsx
+++ src/client/components/pages/objection-comments/ObjectionObjectionCommentsPage.tsx
@@ -1,6 +1,7 @@
 import { Comment } from "@material-ui/icons";
 import React, { useContext } from "react";
 import { useDispatch } from "react-redux";
+import { createPage } from "../../../enhancers/createPage";
 import { useBuffers } from "../../../hooks/useBuffers";
 import { useEntity } from "../../../hooks/useEntity";
 import { actions } from "../../../reducers";
@@ -8,52 +9,54 @@ import { generateBufferId } from "../../../reducers/buffers";
 import { ObjectionCommentEditor } from "../../editors/ObjectionCommentEditor";
 import { ObjectionCommentList } from "../../lists/objection-comments/ObjectionCommentList";
 import { UserContext } from "../../project/Context";
-import { Page } from "../../project/Page";
-import { PageProps } from "../../project/Router";
+import { Loading } from "../../project/Loading";
 import { Button } from "../../ui";
 
-export const ObjectionObjectionCommentsPage = React.memo<PageProps>(props => {
-  const objectionId = props.match.params.id;
+export const ObjectionObjectionCommentsPage = createPage()(
+  React.memo(({ t }) => t("抗議へのコメント")),
+  React.memo(props => {
+    const objectionId = props.match.params.id;
 
-  const dispatch = useDispatch();
-  const currentUser = useContext(UserContext);
+    const dispatch = useDispatch();
+    const currentUser = useContext(UserContext);
 
-  const objectionCommentBuffers = useBuffers("ObjectionComment");
-  const objectionCommentId = Object.keys(objectionCommentBuffers).find(bufferId => {
-    const buffer = objectionCommentBuffers[bufferId];
+    const objectionCommentBuffers = useBuffers("ObjectionComment");
+    const objectionCommentId = Object.keys(objectionCommentBuffers).find(bufferId => {
+      const buffer = objectionCommentBuffers[bufferId];
 
-    return buffer !== undefined && buffer.targetId === objectionId;
-  });
+      return buffer !== undefined && buffer.targetId === objectionId;
+    });
 
-  const onComment = () => {
-    dispatch(
-      actions.buffers.update("ObjectionComment", generateBufferId(), {
-        targetId: objectionId
-      })
-    );
-  };
-
-  const { entity: objection } = useEntity("Objection", objectionId);
-  if (objection === undefined) {
-    return null;
-  }
-
-  const isOwner = currentUser.permission === "Owner";
-  const isOwn = objection.objectorId === currentUser.id;
-
-  if (!isOwner && !isOwn) {
-    return null;
-  }
-
-  return (
-    <Page title="抗議へのコメント">
-      {objectionCommentId === undefined && <Button icon={<Comment />} label="コメントする" onClick={onComment} />}
-      {objectionCommentId !== undefined && <ObjectionCommentEditor bufferId={objectionCommentId} />}
-      <ObjectionCommentList
-        initialParams={{
+    const onComment = () => {
+      dispatch(
+        actions.buffers.update("ObjectionComment", generateBufferId(), {
           targetId: objectionId
-        }}
-      />
-    </Page>
-  );
-});
+        })
+      );
+    };
+
+    const { entity: objection, ...objectionProps } = useEntity("Objection", objectionId);
+    if (objection === undefined) {
+      return <Loading {...objectionProps} />;
+    }
+
+    const isOwner = currentUser.permission === "Owner";
+    const isOwn = objection.objectorId === currentUser.id;
+
+    if (!isOwner && !isOwn) {
+      return null;
+    }
+
+    return (
+      <>
+        {objectionCommentId === undefined && <Button icon={<Comment />} label="コメントする" onClick={onComment} />}
+        {objectionCommentId !== undefined && <ObjectionCommentEditor bufferId={objectionCommentId} />}
+        <ObjectionCommentList
+          initialParams={{
+            targetId: objectionId
+          }}
+        />
+      </>
+    );
+  })
+);
--- src/client/components/pages/report-comments/ReportReportCommentsPage.tsx
+++ src/client/components/pages/report-comments/ReportReportCommentsPage.tsx
@@ -1,6 +1,7 @@
 import { Comment } from "@material-ui/icons";
 import React, { useContext } from "react";
 import { useDispatch } from "react-redux";
+import { createPage } from "../../../enhancers/createPage";
 import { useBuffers } from "../../../hooks/useBuffers";
 import { useEntity } from "../../../hooks/useEntity";
 import { actions } from "../../../reducers";
@@ -8,52 +9,54 @@ import { generateBufferId } from "../../../reducers/buffers";
 import { ReportCommentEditor } from "../../editors/ReportCommentEditor";
 import { ReportCommentList } from "../../lists/report-comments/ReportCommentList";
 import { UserContext } from "../../project/Context";
-import { Page } from "../../project/Page";
-import { PageProps } from "../../project/Router";
+import { Loading } from "../../project/Loading";
 import { Button } from "../../ui";
 
-export const ReportReportCommentsPage = React.memo<PageProps>(props => {
-  const reportId = props.match.params.id;
+export const ReportReportCommentsPage = createPage()(
+  React.memo(({ t }) => t("報告へのコメント")),
+  React.memo(props => {
+    const reportId = props.match.params.id;
 
-  const dispatch = useDispatch();
-  const currentUser = useContext(UserContext);
+    const dispatch = useDispatch();
+    const currentUser = useContext(UserContext);
 
-  const reportCommentBuffers = useBuffers("ReportComment");
-  const reportCommentId = Object.keys(reportCommentBuffers).find(bufferId => {
-    const buffer = reportCommentBuffers[bufferId];
+    const reportCommentBuffers = useBuffers("ReportComment");
+    const reportCommentId = Object.keys(reportCommentBuffers).find(bufferId => {
+      const buffer = reportCommentBuffers[bufferId];
 
-    return buffer !== undefined && buffer.targetId === reportId;
-  });
+      return buffer !== undefined && buffer.targetId === reportId;
+    });
 
-  const onComment = () => {
-    dispatch(
-      actions.buffers.update("ReportComment", generateBufferId(), {
-        targetId: reportId
-      })
-    );
-  };
-
-  const { entity: report } = useEntity("Report", reportId);
-  if (report === undefined) {
-    return null;
-  }
-
-  const isOwner = currentUser.permission === "Owner";
-  const isOwn = report.reporterId === currentUser.id;
-
-  if (!isOwner && !isOwn) {
-    return null;
-  }
-
-  return (
-    <Page title="報告へのコメント">
-      {reportCommentId === undefined && <Button icon={<Comment />} label="コメントする" onClick={onComment} />}
-      {reportCommentId !== undefined && <ReportCommentEditor bufferId={reportCommentId} />}
-      <ReportCommentList
-        initialParams={{
+    const onComment = () => {
+      dispatch(
+        actions.buffers.update("ReportComment", generateBufferId(), {
           targetId: reportId
-        }}
-      />
-    </Page>
-  );
-});
+        })
+      );
+    };
+
+    const { entity: report, ...reportProps } = useEntity("Report", reportId);
+    if (report === undefined) {
+      return <Loading {...reportProps} />;
+    }
+
+    const isOwner = currentUser.permission === "Owner";
+    const isOwn = report.reporterId === currentUser.id;
+
+    if (!isOwner && !isOwn) {
+      return null;
+    }
+
+    return (
+      <>
+        {reportCommentId === undefined && <Button icon={<Comment />} label="コメントする" onClick={onComment} />}
+        {reportCommentId !== undefined && <ReportCommentEditor bufferId={reportCommentId} />}
+        <ReportCommentList
+          initialParams={{
+            targetId: reportId
+          }}
+        />
+      </>
+    );
+  })
+);
