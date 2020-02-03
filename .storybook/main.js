5f3cbd2e5b172b2d3294b6b1bd0aafd484ee1767
bizen241 <bizen241@gmail.com>
2020-02-04 08:17:30 +0900

    test(storybook): add npm script

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+const { resolve } = require("path");
+
+module.exports = {
+  stories: ["../src/**/*.stories.tsx"],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+            configFile: resolve(__dirname, "../config/tsconfig.client.json")
+          }
+        }
+      ]
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+
+    return config;
+  }
+};
