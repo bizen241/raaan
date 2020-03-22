df00afd5cf68b1566b61cfdd37297e34db434994
bizen241 <bizen241@gmail.com>
2020-03-22 20:50:32 +0900

    test(ui): update proxy

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = router =>
+module.exports = router => {
   router.use(
     "/api",
     createProxyMiddleware({
@@ -8,3 +8,11 @@ module.exports = router =>
       changeOrigin: true
     })
   );
+  router.use(
+    "/test",
+    createProxyMiddleware({
+      target: "http://localhost:3000",
+      changeOrigin: true
+    })
+  );
+};
